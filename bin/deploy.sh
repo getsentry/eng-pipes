9b370e2cd6d611118b43aa50d62317d8fe9c5b28
Billy Vong <billy@sentry.io>
2020-05-20 18:30:35 -0700

    build(): Add `--region` for gcp

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,5 +4,5 @@ PROJECT=sentry-dev-tooling
 IMAGE=gcr.io/${PROJECT}/webhooks
 
 gcloud builds submit --tag $IMAGE --project=$PROJECT
-gcloud run deploy webhooks --image $IMAGE --project=$PROJECT --platform managed --allow-unauthenticated
+gcloud run deploy webhooks --image $IMAGE --project=$PROJECT --platform managed --allow-unauthenticated --region=us-west1
 
