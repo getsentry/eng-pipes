58809647d0aeb984d5e34518e2cf95fe4151223c
Billy Vong <billy@sentry.io>
2020-05-20 18:12:29 -0700

    build(): Add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+PROJECT=sentry-dev-tooling
+IMAGE=gcr.io/${PROJECT}/webhooks
+
+gcloud builds submit --tag $IMAGE --project=$PROJECT
+gcloud run deploy webhooks --image $IMAGE --project=$PROJECT --platform managed --allow-unauthenticated
+
