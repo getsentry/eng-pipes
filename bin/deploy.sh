a1ff10b16dc5f9835749664ba156dc2b30ace581
Billy Vong <billy@sentry.io>
2020-05-22 16:04:58 -0700

    err project->product

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PROJECT=super-big-data
-IMAGE=gcr.io/${PROJECT}/project-eng-webhooks
+IMAGE=gcr.io/${PROJECT}/product-eng-webhooks
 
 gcloud builds submit --tag $IMAGE --project=$PROJECT --gcs-log-dir=gs://${PROJECT}_cloudbuild/logs
 gcloud run deploy product-eng-webhooks --image $IMAGE --project=$PROJECT --platform managed --allow-unauthenticated --region=us-west1
