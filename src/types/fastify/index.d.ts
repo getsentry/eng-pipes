fadb136edd93d7a1c775dc6735ce2132d8af2f4b
Billy Vong <billyvg@users.noreply.github.com>
2020-05-28 12:09:51 -0700

    ref(): Refactor route handler (#1)
    Make service dynamic, move logic into `handlers/metrics/:serviceName`

---
+++ src/types/fastify/index.d.ts
@@ -0,0 +1,15 @@
+import * as fastify from 'fastify';
+import * as http from 'http';
+
+declare module 'fastify' {
+  export interface FastifyInstance<
+    HttpServer = http.Server,
+    HttpRequest = http.IncomingMessage,
+    HttpResponse = http.ServerResponse
+  > {
+    notFound(
+      request: fastify.FastifyRequest,
+      reply: fastify.FastifyReply
+    ): void;
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,6 @@
 import travisPayload from '@test/travis.json';
 
 import { buildServer } from '@app/buildServer';
-import { insert } from '@app/db';
-import { verifyTravisWebhook } from '@app/verifyTravisWebhook';
-
-jest.mock('@app/verifyTravisWebhook', () => ({
-  verifyTravisWebhook: jest.fn(() => true),
-}));
-
-jest.mock('@app/db', () => ({
-  insert: jest.fn(() => []),
-}));
 
 describe('travis webhook', function() {
   let fastify;
@@ -22,58 +12,13 @@ describe('travis webhook', function() {
     fastify.close();
   });
 
-  it('correct inserts travis webhook', async function() {
+  it('returns 404 when handler not found', async function() {
     const response = await fastify.inject({
       method: 'POST',
-      url: '/metrics/travis/webhook',
+      url: '/metrics/..test/webhook',
       payload: { payload: JSON.stringify(travisPayload) },
     });
 
-    expect(response.statusCode).toBe(200);
-    expect(insert).toHaveBeenCalledWith({
-      event: 'build_started',
-      meta: {
-        base_commit: '704b6b8cae9023275785f8a752025d117e788f38',
-        head_commit: 'e2fb88b6df64a87c2cc78256a50a1e0fe1fbefd2',
-        pull_request_title: 'update travis config',
-      },
-      object_id: 11,
-      source_id: 684354870,
-      source: 'travis',
-      start_timestamp: '2020-05-13T23:43:52Z',
-      end_timestamp: null,
-    });
-
-    expect(insert).toHaveBeenCalledWith({
-      event: 'build_started',
-      meta: {
-        name: 'Backend',
-        base_commit: '704b6b8cae9023275785f8a752025d117e788f38',
-        head_commit: 'e2fb88b6df64a87c2cc78256a50a1e0fe1fbefd2',
-        pull_request_title: 'update travis config',
-      },
-      object_id: 11,
-      source_id: 684354871,
-      parent_id: 684354870,
-      source: 'travis',
-      start_timestamp: '2020-05-13T23:43:52Z',
-      end_timestamp: null,
-    });
-
-    expect(insert).toHaveBeenCalledWith({
-      event: 'build_passed',
-      meta: {
-        name: 'Frontend [test]',
-        base_commit: '704b6b8cae9023275785f8a752025d117e788f38',
-        head_commit: 'e2fb88b6df64a87c2cc78256a50a1e0fe1fbefd2',
-        pull_request_title: 'update travis config',
-      },
-      object_id: 11,
-      source_id: 684354872,
-      parent_id: 684354870,
-      source: 'travis',
-      start_timestamp: '2020-05-15T20:56:26Z',
-      end_timestamp: '2020-05-15T20:59:02Z',
-    });
+    expect(response.statusCode).toBe(404);
   });
 });
