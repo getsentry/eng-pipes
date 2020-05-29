ccf008760556c9b7d91beadb02924289c22aa45f
Billy Vong <billy@sentry.io>
2020-05-29 11:33:54 -0700

    build(github): Fix failed builds still go green
    If image builds fail but deploy succeeds, the github action still passes.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,6 @@
 PROJECT=super-big-data
 IMAGE=gcr.io/${PROJECT}/product-eng-webhooks
 
-gcloud builds submit --tag $IMAGE --project=$PROJECT --gcs-log-dir=gs://${PROJECT}_cloudbuild/logs
+gcloud builds submit --tag $IMAGE --project=$PROJECT --gcs-log-dir=gs://${PROJECT}_cloudbuild/logs && \
 gcloud run deploy product-eng-webhooks --image $IMAGE --project=$PROJECT --platform managed --allow-unauthenticated --region=us-west1
 
