12f42ccd50f8869c353758cd39a55edc4a79b353
Billy Vong <billy@sentry.io>
2020-06-07 22:57:57 -0700

    build(deploy): Fix deploy to include `GH_WEBHOOK_SECRET`

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,5 +4,5 @@ PROJECT=super-big-data
 IMAGE=gcr.io/${PROJECT}/product-eng-webhooks
 
 gcloud builds submit --tag $IMAGE --project=$PROJECT --gcs-log-dir=gs://${PROJECT}_cloudbuild/logs && \
-gcloud run deploy product-eng-webhooks --image $IMAGE --set-env-vars=GH_APP_IDENTIFIER="${GH_APP_IDENTIFIER}",GH_APP_SECRET_KEY="${GH_APP_SECRET_KEY}" --project=$PROJECT --platform managed --allow-unauthenticated --region=us-west1
+gcloud run deploy product-eng-webhooks --image $IMAGE --set-env-vars=GH_APP_IDENTIFIER="${GH_APP_IDENTIFIER}",GH_APP_SECRET_KEY="${GH_APP_SECRET_KEY}",GH_WEBHOOK_SECRET="${GH_WEBHOOK_SECRET}" --project=$PROJECT --platform managed --allow-unauthenticated --region=us-west1
 
