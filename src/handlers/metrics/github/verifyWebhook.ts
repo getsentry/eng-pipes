9bee1efccc6d30e8d4aaa821bfb07619d848bae0
Billy Vong <billy@sentry.io>
2020-06-08 09:57:20 -0700

    debug verifyWebhooks

--- src/handlers/metrics/github/verifyWebhook.ts
+++ src/handlers/metrics/github/verifyWebhook.ts
@@ -2,12 +2,6 @@ import crypto from 'crypto';
 
 import fastify from 'fastify';
 
-const SECRET = process.env.GH_WEBHOOK_SECRET || '';
-
-if (!SECRET) {
-  throw new Error('GH_WEBHOOK_SECRET is not set');
-}
-
 export function verifyWebhook(request: fastify.FastifyRequest) {
   // XXX: `fastify` does not offer a "raw body" and instead provides us with the decoded payload
   // This is not ideal because of 1) extra processing, 2) potential signature mismatches, 3) potential attack vectors
@@ -15,6 +9,15 @@ export function verifyWebhook(request: fastify.FastifyRequest) {
   // See https://github.com/fastify/fastify/issues/707
   const payload = JSON.stringify(request.body);
   const sig = request.headers['x-hub-signature'] || '';
+  const SECRET = process.env.GH_WEBHOOK_SECRET || '';
+  if (!SECRET) {
+    console.error('GH_WEBHOOK_SECRET is not set');
+  }
+
+  if (!sig) {
+    console.error('No `x-hub-signature` header found');
+  }
+
   const hmac = crypto.createHmac('sha1', SECRET);
   const digest = Buffer.from(
     `sha1=${hmac.update(payload).digest('hex')}`,
