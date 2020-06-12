21ccddc2805551b22f65a184bb35304aca0a84a9
Billy Vong <billy@sentry.io>
2020-06-11 17:10:48 -0700

    build(google): Refactor and add `VERSION` env var for Sentry releases

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,10 +3,17 @@
 PROJECT=super-big-data
 IMAGE=gcr.io/${PROJECT}/product-eng-webhooks
 
+env_vars=ENV=production,
+env_vars+=GH_APP_IDENTIFIER="${GH_APP_IDENTIFIER}",
+env_vars+=GH_APP_SECRET_KEY="${GH_APP_SECRET_KEY}",
+env_vars+=GH_WEBHOOK_SECRET="${GH_WEBHOOK_SECRET}",
+env_vars+=VERSION="${VERSION}",
+env_vars+=SENTRY_WEBPACK_WEBHOOK_SECRET="${SENTRY_WEBPACK_WEBHOOK_SECRET}",
+
 gcloud builds submit --tag $IMAGE --project=$PROJECT --gcs-log-dir=gs://${PROJECT}_cloudbuild/logs && \
 gcloud run deploy product-eng-webhooks \
   --image $IMAGE \
-  --set-env-vars=ENV=production,GH_APP_IDENTIFIER="${GH_APP_IDENTIFIER}",GH_APP_SECRET_KEY="${GH_APP_SECRET_KEY}",GH_WEBHOOK_SECRET="${GH_WEBHOOK_SECRET}",SENTRY_WEBPACK_WEBHOOK_SECRET="${SENTRY_WEBPACK_WEBHOOK_SECRET}" \
+  --set-env-vars=$env_vars \
   --project=$PROJECT \
   --platform managed \
   --allow-unauthenticated \
