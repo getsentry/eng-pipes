aaa894c66453483ad58fbb9612809c3032254724
Billy Vong <billy@sentry.io>
2020-10-28 11:35:52 -0700

    feat(metrics): Add endpoint for bootstrap dev env
    This records metrics for bootstrap dev env script

---
+++ src/handlers/metrics/bootstrap-dev-env/bootstrap-dev-env.ts
@@ -0,0 +1,22 @@
+import { FastifyRequest } from 'fastify';
+
+import { insert } from '../../../utils/db';
+
+export async function handler(request: FastifyRequest) {
+  const { body: payload } = request;
+
+  const now = new Date();
+
+  insert({
+    source: 'bootstrap-dev-env',
+    event: payload.event,
+    // timestamps can be null if it has not completed yet
+    start_timestamp: payload.event === 'bootstrap_start' ? now : null,
+    end_timestamp: payload.event === 'bootstrap_end' ? now : null,
+    meta: {
+      username: payload.name,
+    },
+  });
+
+  return {};
+}
---
+++ src/handlers/metrics/bootstrap-dev-env/index.ts
@@ -0,0 +1,1 @@
+export { handler } from './bootstrap-dev-env';
