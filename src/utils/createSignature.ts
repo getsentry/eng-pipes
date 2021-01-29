e235488796577a6c11b1a214ff0330ecdc371691
Billy Vong <billy@sentry.io>
2021-01-29 14:55:21 -0800

    test: Add tests for slack typescript cmd

--- src/utils/createSignature.ts
+++ src/utils/createSignature.ts
@@ -3,9 +3,10 @@ import crypto from 'crypto';
 export function createSignature(
   payload: string,
   secret: string,
-  createDigest = (i) => i
+  createDigest = (i) => i,
+  method: 'sha1' | 'sha256' = 'sha1'
 ) {
-  const hmac = crypto.createHmac('sha1', secret);
+  const hmac = crypto.createHmac(method, secret);
   const digest = Buffer.from(
     createDigest(hmac.update(payload).digest('hex')),
     'utf8'
