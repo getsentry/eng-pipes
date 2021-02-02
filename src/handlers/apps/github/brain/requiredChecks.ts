d0579695b7f039c33b4122dadba0a33b6aca1f09
Billy Vong <billy@sentry.io>
2021-02-02 10:00:02 -0800

    feat(gh): Remove listener before listening
    This fixes a potential bug with the listener being attached multiple
    times. Also fix failed jobs message to include more failure conclusions.

--- src/handlers/apps/github/brain/requiredChecks.ts
+++ src/handlers/apps/github/brain/requiredChecks.ts
@@ -21,51 +21,50 @@ function githubMdToSlack(str: string) {
   return str;
 }
 
-export async function requiredChecks() {
-  githubEvents.on('check_run', ({ payload }) => {
-    // Only on `getsentry` repo
-    if (payload.repository?.full_name !== 'getsentry/getsentry') {
-      return;
-    }
+function handler({ payload }) {
+  // Only on `getsentry` repo
+  if (payload.repository?.full_name !== 'getsentry/getsentry') {
+    return;
+  }
 
-    const { check_run: checkRun } = payload;
+  const { check_run: checkRun } = payload;
 
-    // Only care about completed checks
-    if (checkRun.status !== 'completed') {
-      return;
-    }
+  // Only care about completed checks
+  if (checkRun.status !== 'completed') {
+    return;
+  }
 
-    if (checkRun.name !== REQUIRED_CHECK_NAME) {
-      return;
-    }
+  if (checkRun.name !== REQUIRED_CHECK_NAME) {
+    return;
+  }
 
-    // Conclusion can be one of:
-    //   success, failure, neutral, cancelled, skipped, timed_out, or action_required
-    //
-    // Ignore "successful" conclusions
-    if (OK_CONCLUSIONS.includes(checkRun.conclusion || '')) {
-      return;
-    }
+  // Conclusion can be one of:
+  //   success, failure, neutral, cancelled, skipped, timed_out, or action_required
+  //
+  // Ignore "successful" conclusions
+  if (OK_CONCLUSIONS.includes(checkRun.conclusion || '')) {
+    return;
+  }
 
-    // Otherwise, there is a failed check
-    // Need to notify channel that the build has failed
-    // We need to include:
-    // 1) A link to the getsentry commit
-    //   1a) a link to the sentry commit if possible
-    // 2) The author of the failed commit (will need to lookup their slack user from their gh email)
-    // 3) A list of the failed checks (job name, duration, status)
-    // 4) Button to re-run job
-    const failedJobs = checkRun.output?.text
-      ?.split('\n')
-      .filter((text) => text.startsWith('|'))
-      .map((text) => text.split('|').filter(Boolean)) // Split and filter out empty els
-      .filter(([, conclusion]) => conclusion.includes('failure'));
+  // Otherwise, there is a failed check
+  // Need to notify channel that the build has failed
+  // We need to include:
+  // 1) A link to the getsentry commit
+  //   1a) a link to the sentry commit if possible
+  // 2) The author of the failed commit (will need to lookup their slack user from their gh email)
+  // 3) A list of the failed checks (job name, duration, status)
+  // 4) Button to re-run job
+  const failedJobs = checkRun.output?.text
+    ?.split('\n')
+    .filter((text) => text.startsWith('|'))
+    .map((text) => text.split('|').filter(Boolean)) // Split and filter out empty els
+    .filter(([, conclusion]) => !OK_CONCLUSIONS.includes(conclusion));
 
-    const commitLink = `https://github.com/${OWNER}/${GETSENTRY_REPO}/commits/${checkRun.head_sha}`;
-    const commitLinkText = `Commit ${checkRun.head_sha.slice(0, 7)}`;
-    const message = `<${commitLink}|${commitLinkText}> failed (<${
-      checkRun.html_url
-    }|View Build>)
+  const commitLink = `https://github.com/${OWNER}/${GETSENTRY_REPO}/commits/${checkRun.head_sha}`;
+  const commitLinkText = `Commit ${checkRun.head_sha.slice(0, 7)}`;
+  const message = `<${commitLink}|${commitLinkText}> failed (<${
+    checkRun.html_url
+  }|View Build>)
 
 ${failedJobs
   ?.map(
@@ -73,25 +72,27 @@ ${failedJobs
   )
   .join('\n')}`;
 
-    console.log(message);
-
-    web.chat.postMessage({
-      channel: SLACK_NOTIFICATION_CHANNEL,
-      text: `Build has failed on ${OWNER}/${GETSENTRY_REPO}@master`,
-      attachments: [
-        {
-          color: '#F55459',
-          blocks: [
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: message,
-              },
+  web.chat.postMessage({
+    channel: SLACK_NOTIFICATION_CHANNEL,
+    text: `Build has failed on ${OWNER}/${GETSENTRY_REPO}@master`,
+    attachments: [
+      {
+        color: '#F55459',
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: message,
             },
-          ],
-        },
-      ],
-    });
+          },
+        ],
+      },
+    ],
   });
+}
+
+export async function requiredChecks() {
+  githubEvents.removeListener('check_run', handler);
+  githubEvents.on('check_run', handler);
 }
