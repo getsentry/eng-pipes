8d894ac5c8556e3dc159ba3a53d1fbfaab2c15e9
Billy Vong <billy@sentry.io>
2021-02-09 15:34:43 -0800

    feat(): Add CloudSQL support via knex
    Add workflow to allow running migrations via workflow dispatch

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,6 +11,11 @@ env_vars="${env_vars}SLACK_SIGNING_SECRET=${SLACK_SIGNING_SECRET},"
 env_vars="${env_vars}SLACK_ACCESS_TOKEN=${SLACK_ACCESS_TOKEN},"
 env_vars="${env_vars}VERSION=${VERSION},"
 env_vars="${env_vars}SENTRY_WEBPACK_WEBHOOK_SECRET=${SENTRY_WEBPACK_WEBHOOK_SECRET},"
+env_vars="${env_vars}DB_USER=${DB_USER},"
+env_vars="${env_vars}DB_NAME=${DB_NAME},"
+env_vars="${env_vars}DB_PASSWORD=${DB_PASSWORD},"
+env_vars="${env_vars}DB_INSTANCE_CONNECTION_NAME=${DB_INSTANCE_CONNECTION_NAME},"
+
 
 gcloud builds submit --tag $IMAGE --project=$PROJECT --gcs-log-dir=gs://${PROJECT}_cloudbuild/logs && \
 gcloud run deploy product-eng-webhooks \
@@ -19,6 +24,7 @@ gcloud run deploy product-eng-webhooks \
   --project=$PROJECT \
   --platform managed \
   --allow-unauthenticated \
+  --add-cloudsql-instances ${DB_INSTANCE_CONNECTION_NAME} \
   --region=us-west1
 
 
---
+++ knexfile.ts
@@ -0,0 +1,57 @@
+// Update with your config settings.
+import {
+  DB_HOST,
+  DB_INSTANCE_CONNECTION_NAME,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_USER,
+} from './src/config';
+
+module.exports = {
+  local: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres',
+      user: 'postgres',
+      password: 'docker',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres',
+      user: 'postgres',
+      password: DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: `${DB_HOST}/${DB_INSTANCE_CONNECTION_NAME}`,
+      database: DB_NAME,
+      user: DB_USER,
+      password: DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
