2720708b8c26d0d6557537402d6ce65ed2c4a5b5
Billy Vong <billy@sentry.io>
2021-02-11 09:26:06 -0800

    ref: Remove `web2` hack
    I was using another bot token to access user profiles, but I fixed the issue by revoking and reinstalling the App. We were using an outdated token type.

--- src/api/slack/index.ts
+++ src/api/slack/index.ts
@@ -3,14 +3,6 @@ import { WebClient } from '@slack/web-api';
 
 import { SLACK_BOT_USER_ACCESS_TOKEN, SLACK_SIGNING_SECRET } from '@app/config';
 
-// XXX(billy): Uhhh for some reason our normal bot token
-// (for app Sentry Bot: https://api.slack.com/apps/ASUD2NK2S)
-// does not work for fetching user profiles.
-// Instead we are using DogBot: https://api.slack.com/apps/A0182D08F9T/oauth?
-//
-// I currently have a support ticket with slack about this
-const web2 = new WebClient(process.env.SLACK_BOT_USER_ACCESS_TOKEN_TEMP);
-
 const bolt = new App({
   token: SLACK_BOT_USER_ACCESS_TOKEN,
   signingSecret: SLACK_SIGNING_SECRET,
@@ -21,7 +13,7 @@ const bolt = new App({
 // This is to support multiple workspaces, see https://github.com/slackapi/bolt-js/issues/250
 bolt.client = new WebClient(SLACK_BOT_USER_ACCESS_TOKEN);
 
-export { web2, bolt };
+export { bolt };
 
 // @ts-ignore
 bolt.error((error) => {
