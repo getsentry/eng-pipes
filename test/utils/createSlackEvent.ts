feb78bc0050661ad31d112ece01903f8a359405f
Billy Vong <billy@sentry.io>
2021-02-14 14:37:49 -0800

    test: Refactor slack mock events

--- test/utils/createSlackEvent.ts
+++ test/utils/createSlackEvent.ts
@@ -1,3 +1,5 @@
+import { getSlackEvent } from '@test/utils/getSlackEvent';
+
 import { Fastify } from '@app/types';
 import { createSignature } from '@utils/createSignature';
 
@@ -9,9 +11,40 @@ function createSlackSignature(payload, timestamp) {
     'sha256'
   );
 }
-export async function createSlackEvent(fastify: Fastify, payload) {
+
+const DEFAULT_PAYLOAD = {
+  token: 'token',
+  team_id: 'T018UAQ7YRW',
+  api_app_id: 'A017XPC80S2',
+  type: 'event_callback',
+  event_id: 'Ev01NF8EPNU9',
+  event_time: 1613340527,
+  authed_users: ['U018UAXJVG8'],
+  authorizations: [
+    {
+      enterprise_id: null,
+      team_id: 'T018UAQ7YRW',
+      user_id: 'U018UAXJVG8',
+      is_bot: true,
+      is_enterprise_install: false,
+    },
+  ],
+  is_ext_shared_channel: false,
+};
+
+export async function createSlackEvent(
+  fastify: Fastify,
+  event: string,
+  payload: Record<string, any>
+) {
   const now = +new Date();
-  const signature = createSlackSignature(payload, now);
+
+  const fullPayload = {
+    ...DEFAULT_PAYLOAD,
+    event: getSlackEvent(event, payload),
+  };
+
+  const signature = createSlackSignature(fullPayload, now);
 
   return await fastify.inject({
     method: 'POST',
@@ -20,6 +53,6 @@ export async function createSlackEvent(fastify: Fastify, payload) {
       'x-slack-request-timestamp': now,
       'x-slack-signature': `v0=${signature}`,
     },
-    payload,
+    payload: fullPayload,
   });
 }
