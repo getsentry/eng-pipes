dabb37a0aa7260c9ae28b37a7947f8df5c84d71d
Billy Vong <billy@sentry.io>
2021-02-17 17:01:47 -0800

    add src/types

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
 // eslint-disable-next-line
 module.exports = {
-  collectCoverageFrom: ['**/*.ts', '!test/**', '!migrations/**'],
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!test/**',
+    '!migrations/**',
+    '!src/types/**',
+  ],
   testEnvironment: 'node',
   testRunner: 'jest-circus/runner',
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
