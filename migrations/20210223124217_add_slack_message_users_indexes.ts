a122d5df3edab8cce51265713f6c2aabb29d91ca
Billy Vong <billyvg@users.noreply.github.com>
2021-02-23 14:04:12 -0800

    feat(migration): Add indexes to users and slack messages tables (#117)

---
+++ migrations/20210223124217_add_slack_message_users_indexes.ts
@@ -0,0 +1,29 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.index(['email', 'slackUser', 'githubUser']);
+  });
+
+  await knex.schema.alterTable('slack_messages', (table) => {
+    table.index(['refId']);
+  });
+
+  await knex.schema.alterTable('required_checks_status', (table) => {
+    table.index(['ref']);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.dropIndex(['email', 'slackUser', 'githubUser']);
+  });
+
+  await knex.schema.alterTable('slack_messages', (table) => {
+    table.dropIndex(['refId']);
+  });
+
+  await knex.schema.alterTable('required_checks_status', (table) => {
+    table.dropIndex(['ref']);
+  });
+}
