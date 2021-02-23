35da68e8c77dc1cea2cd3525456966392c54661e
Billy Vong <billyvg@users.noreply.github.com>
2021-02-23 11:15:44 -0800

    feat: Add Slack command to sync Slack users to DB (#67)
    This fetches Sentry employees Slack user to database (if they have a GH login name defined in their slack profile).

---
+++ src/brain/syncSlackUsers/README.md
@@ -0,0 +1,4 @@
+# syncSlackUsers
+
+This fetches a list of Slack users, ensures they are active and have `@sentry.io` e-mail addresses, checks for their GitHub profile field (in Slack), and updates database linking their Slack user to GitHub login
+
---
+++ src/brain/syncSlackUsers/index.ts
@@ -0,0 +1,72 @@
+import { SLACK_PROFILE_ID_GITHUB } from '@/config';
+import { bolt } from '@api/slack';
+import { db } from '@utils/db';
+import { isSentrySlackUser } from '@utils/isSentrySlackUser';
+
+export function syncSlackUsers() {
+  bolt.event('app_mention', async ({ event, say, client }) => {
+    if (!event.text.includes('sync slack users')) {
+      return;
+    }
+
+    let total = 0;
+
+    const msgPromise = say(':sentry-loading: Syncing Slack users...');
+
+    async function processUsers(cursor?: string) {
+      const results = await client.users.list({ cursor, limit: 10 });
+
+      // @ts-ignore
+      for (const member of results.members) {
+        if (!isSentrySlackUser(member)) {
+          continue;
+        }
+
+        const profile = await client.users.profile.get({
+          user: member.id,
+        });
+
+        // @ts-ignore
+        const githubLogin = profile?.profile.fields?.[
+          SLACK_PROFILE_ID_GITHUB
+        ]?.value.replace(/(https:\/\/|)github.com\//, '');
+
+        const exists = await db('users')
+          .where({
+            email: member.profile?.email,
+            slackUser: member.id,
+          })
+          .first('*');
+
+        // If user doesn't already exist *OR* they do not have a github user set, update db
+        if (!exists || (!exists.githubUser && githubLogin)) {
+          total++;
+          await db('users')
+            .insert({
+              email: member.profile?.email,
+              slackUser: member.id,
+              githubUser: githubLogin,
+            })
+            .onConflict(['email'])
+            .merge();
+        }
+      }
+
+      if (results.response_metadata?.next_cursor) {
+        await processUsers(results.response_metadata?.next_cursor);
+      } else {
+        console.log('done');
+        return;
+      }
+    }
+    await processUsers();
+
+    const msg = await msgPromise;
+
+    await client.chat.update({
+      ts: `${msg.ts}`,
+      channel: `${msg.channel}`,
+      text: `Updated ${total} users`,
+    });
+  });
+}
