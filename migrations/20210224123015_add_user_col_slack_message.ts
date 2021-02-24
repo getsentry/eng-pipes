1684df089ccdb38951862c905d0cfe457aa62501
Billy Vong <billyvg@users.noreply.github.com>
2021-02-24 12:42:38 -0800

    feat(migration): Add `user` column to `slack_messages` table (#126)
    The `channel` argument to `postMessage` is not the same as the result of `postMessage()` `channel`. We need to store the user somewhere.

---
+++ migrations/20210224123015_add_user_col_slack_message.ts
@@ -0,0 +1,13 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  knex.schema.table('slack_messages', (table) => {
+    table.string('user');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.table('slack_messages', (table) => {
+    table.dropColumn('user');
+  });
+}
