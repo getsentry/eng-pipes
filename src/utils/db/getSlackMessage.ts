0cc988746eae0bbd44f4689e4ce8aa9bb8e61dd3
Billy Vong <billyvg@users.noreply.github.com>
2021-02-25 12:33:30 -0800

    ref(util): Refactor `getSlackMessage` to support multiple refIds (#134)
    Change `getSlackMessage` to support multiple `refId`s, update `updateDeployNotifications` to use this fn.

--- src/utils/db/getSlackMessage.ts
+++ src/utils/db/getSlackMessage.ts
@@ -1,6 +1,26 @@
+import { SlackMessageRow } from 'knex/types/tables';
+
 import { SlackMessage } from '@/config/slackMessage';
 import { db } from '@utils/db';
 
-export async function getSlackMessage(type: SlackMessage, refId: string) {
-  return await db('slack_messages').where({ refId, type }).first('*');
+export async function getSlackMessage(
+  type: SlackMessage,
+  refIds: string
+): Promise<SlackMessageRow>;
+export async function getSlackMessage(
+  type: SlackMessage,
+  refIds: string[]
+): Promise<SlackMessageRow[]>;
+export async function getSlackMessage(
+  type: SlackMessage,
+  refIds: string | string[]
+): Promise<SlackMessageRow | SlackMessageRow[]> {
+  if (typeof refIds === 'string') {
+    return await db('slack_messages').where({ type, refId: refIds }).first('*');
+  } else {
+    return await db('slack_messages')
+      .where({ type })
+      .whereIn('refId', refIds)
+      .select('*');
+  }
 }
