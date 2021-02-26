39358c373792b5c17edb9696bfd686be5dbcbf0d
Chad Whitacre <chadwhitacre@sentry.io>
2021-02-26 10:44:41 -0500

    Revert "user_type take 3 (#139)"
    This reverts commit 0d102536a694f1c01ed47bd7dffc8c6dbf72e5d6.

--- src/api/github/getClient.ts
+++ src/api/github/getClient.ts
@@ -1,8 +1,11 @@
 import { createAppAuth } from '@octokit/auth-app';
 import { Octokit } from '@octokit/rest';
 
-function _getClient(installationId?: number) {
-  return new Octokit({
+/**
+ * get API client given an app installation id
+ */
+const getOctokitClient = (installationId?: number) =>
+  new Octokit({
     authStrategy: createAppAuth,
     auth: {
       // Initialize GitHub App with id:private_key pair and generate JWT which is used for
@@ -11,27 +14,29 @@ function _getClient(installationId?: number) {
       installationId,
     },
   });
-}
 
 /**
- * Return an Octokit client. Owner is required. If repo is given, the client
- * will use installation auth bound to the repo, otherwise owner is assumed to
- * be an org and the client will use installation auth bound to the org.
+ * Fetch an app installation instance given an owner and repo
  */
-export async function getClient(owner: string, repo?: string) {
+export async function getClient(owner: string, repo: string) {
   if (!process.env.GH_APP_SECRET_KEY) {
     throw new Error('GH_APP_SECRET_KEY not defined');
   }
+
   if (!process.env.GH_APP_IDENTIFIER) {
     throw new Error('GH_APP_IDENTIFIER not defined');
   }
 
-  const client = _getClient();
-  let installation;
-  if (repo) {
-    installation = await client.apps.getRepoInstallation({ owner, repo });
-  } else {
-    installation = await client.apps.getOrgInstallation({ org: owner });
-  }
-  return _getClient(installation.data.id);
+  // Client for App (no installation id)
+  const client = getOctokitClient();
+
+  const result = await client.apps.getRepoInstallation({
+    owner,
+    repo,
+  });
+
+  const installationId = result.data.id;
+
+  // Return client for the repo
+  return getOctokitClient(installationId);
 }
--- test/payloads/github/issues.ts
+++ test/payloads/github/issues.ts
@@ -1,42 +1,46 @@
 // Derived from GitHub's docs:
 //
 //   https://docs.github.com/en/developers/webhooks-and-events/webhook-events-and-payloads#webhook-payload-example-when-someone-edits-an-issue.
+//
+// Changed action from edited to opened, unassigned and unmilestoned it. ¯\_(ツ)_/¯
 
 export default {
   action: 'opened',
   issue: {
-    url: 'https://api.github.com/repos/Enterprise/Hello-World/issues/1',
-    repository_url: 'https://api.github.com/repos/Enterprise/Hello-World',
+    url: 'https://api.github.com/repos/Codertocat/Hello-World/issues/1',
+    repository_url: 'https://api.github.com/repos/Codertocat/Hello-World',
     labels_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/issues/1/labels{/name}',
+      'https://api.github.com/repos/Codertocat/Hello-World/issues/1/labels{/name}',
     comments_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/issues/1/comments',
+      'https://api.github.com/repos/Codertocat/Hello-World/issues/1/comments',
     events_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/issues/1/events',
-    html_url: 'https://github.com/Enterprise/Hello-World/issues/1',
+      'https://api.github.com/repos/Codertocat/Hello-World/issues/1/events',
+    html_url: 'https://github.com/Codertocat/Hello-World/issues/1',
     id: 444500041,
     node_id: 'MDU6SXNzdWU0NDQ1MDAwNDE=',
     number: 1,
     title: 'Spelling error in the README file',
     user: {
-      login: 'Picard',
+      login: 'Codertocat',
       id: 21031067,
       node_id: 'MDQ6VXNlcjIxMDMxMDY3',
       avatar_url: 'https://avatars1.githubusercontent.com/u/21031067?v=4',
       gravatar_id: '',
-      url: 'https://api.github.com/users/Picard',
-      html_url: 'https://github.com/Picard',
-      followers_url: 'https://api.github.com/users/Picard/followers',
+      url: 'https://api.github.com/users/Codertocat',
+      html_url: 'https://github.com/Codertocat',
+      followers_url: 'https://api.github.com/users/Codertocat/followers',
       following_url:
-        'https://api.github.com/users/Picard/following{/other_user}',
-      gists_url: 'https://api.github.com/users/Picard/gists{/gist_id}',
-      starred_url: 'https://api.github.com/users/Picard/starred{/owner}{/repo}',
-      subscriptions_url: 'https://api.github.com/users/Picard/subscriptions',
-      organizations_url: 'https://api.github.com/users/Picard/orgs',
-      repos_url: 'https://api.github.com/users/Picard/repos',
-      events_url: 'https://api.github.com/users/Picard/events{/privacy}',
+        'https://api.github.com/users/Codertocat/following{/other_user}',
+      gists_url: 'https://api.github.com/users/Codertocat/gists{/gist_id}',
+      starred_url:
+        'https://api.github.com/users/Codertocat/starred{/owner}{/repo}',
+      subscriptions_url:
+        'https://api.github.com/users/Codertocat/subscriptions',
+      organizations_url: 'https://api.github.com/users/Codertocat/orgs',
+      repos_url: 'https://api.github.com/users/Codertocat/repos',
+      events_url: 'https://api.github.com/users/Codertocat/events{/privacy}',
       received_events_url:
-        'https://api.github.com/users/Picard/received_events',
+        'https://api.github.com/users/Codertocat/received_events',
       type: 'User',
       site_admin: false,
     },
@@ -44,7 +48,7 @@ export default {
       {
         id: 1362934389,
         node_id: 'MDU6TGFiZWwxMzYyOTM0Mzg5',
-        url: 'https://api.github.com/repos/Enterprise/Hello-World/labels/bug',
+        url: 'https://api.github.com/repos/Codertocat/Hello-World/labels/bug',
         name: 'bug',
         color: 'd73a4a',
         default: true,
@@ -67,109 +71,109 @@ export default {
     id: 186853002,
     node_id: 'MDEwOlJlcG9zaXRvcnkxODY4NTMwMDI=',
     name: 'Hello-World',
-    full_name: 'Enterprise/Hello-World',
+    full_name: 'Codertocat/Hello-World',
     private: false,
     owner: {
-      login: 'Enterprise',
+      login: 'Codertocat',
       id: 21031067,
       node_id: 'MDQ6VXNlcjIxMDMxMDY3',
       avatar_url: 'https://avatars1.githubusercontent.com/u/21031067?v=4',
       gravatar_id: '',
-      url: 'https://api.github.com/users/Enterprise',
-      html_url: 'https://github.com/Enterprise',
-      followers_url: 'https://api.github.com/users/Enterprise/followers',
+      url: 'https://api.github.com/users/Codertocat',
+      html_url: 'https://github.com/Codertocat',
+      followers_url: 'https://api.github.com/users/Codertocat/followers',
       following_url:
-        'https://api.github.com/users/Enterprise/following{/other_user}',
-      gists_url: 'https://api.github.com/users/Enterprise/gists{/gist_id}',
+        'https://api.github.com/users/Codertocat/following{/other_user}',
+      gists_url: 'https://api.github.com/users/Codertocat/gists{/gist_id}',
       starred_url:
-        'https://api.github.com/users/Enterprise/starred{/owner}{/repo}',
+        'https://api.github.com/users/Codertocat/starred{/owner}{/repo}',
       subscriptions_url:
-        'https://api.github.com/users/Enterprise/subscriptions',
-      organizations_url: 'https://api.github.com/users/Enterprise/orgs',
-      repos_url: 'https://api.github.com/users/Enterprise/repos',
-      events_url: 'https://api.github.com/users/Enterprise/events{/privacy}',
+        'https://api.github.com/users/Codertocat/subscriptions',
+      organizations_url: 'https://api.github.com/users/Codertocat/orgs',
+      repos_url: 'https://api.github.com/users/Codertocat/repos',
+      events_url: 'https://api.github.com/users/Codertocat/events{/privacy}',
       received_events_url:
-        'https://api.github.com/users/Enterprise/received_events',
-      type: 'Organization',
+        'https://api.github.com/users/Codertocat/received_events',
+      type: 'User',
       site_admin: false,
     },
-    html_url: 'https://github.com/Enterprise/Hello-World',
+    html_url: 'https://github.com/Codertocat/Hello-World',
     description: null,
     fork: false,
-    url: 'https://api.github.com/repos/Enterprise/Hello-World',
-    forks_url: 'https://api.github.com/repos/Enterprise/Hello-World/forks',
+    url: 'https://api.github.com/repos/Codertocat/Hello-World',
+    forks_url: 'https://api.github.com/repos/Codertocat/Hello-World/forks',
     keys_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/keys{/key_id}',
+      'https://api.github.com/repos/Codertocat/Hello-World/keys{/key_id}',
     collaborators_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/collaborators{/collaborator}',
-    teams_url: 'https://api.github.com/repos/Enterprise/Hello-World/teams',
-    hooks_url: 'https://api.github.com/repos/Enterprise/Hello-World/hooks',
+      'https://api.github.com/repos/Codertocat/Hello-World/collaborators{/collaborator}',
+    teams_url: 'https://api.github.com/repos/Codertocat/Hello-World/teams',
+    hooks_url: 'https://api.github.com/repos/Codertocat/Hello-World/hooks',
     issue_events_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/issues/events{/number}',
-    events_url: 'https://api.github.com/repos/Enterprise/Hello-World/events',
+      'https://api.github.com/repos/Codertocat/Hello-World/issues/events{/number}',
+    events_url: 'https://api.github.com/repos/Codertocat/Hello-World/events',
     assignees_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/assignees{/user}',
+      'https://api.github.com/repos/Codertocat/Hello-World/assignees{/user}',
     branches_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/branches{/branch}',
-    tags_url: 'https://api.github.com/repos/Enterprise/Hello-World/tags',
+      'https://api.github.com/repos/Codertocat/Hello-World/branches{/branch}',
+    tags_url: 'https://api.github.com/repos/Codertocat/Hello-World/tags',
     blobs_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/git/blobs{/sha}',
+      'https://api.github.com/repos/Codertocat/Hello-World/git/blobs{/sha}',
     git_tags_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/git/tags{/sha}',
+      'https://api.github.com/repos/Codertocat/Hello-World/git/tags{/sha}',
     git_refs_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/git/refs{/sha}',
+      'https://api.github.com/repos/Codertocat/Hello-World/git/refs{/sha}',
     trees_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/git/trees{/sha}',
+      'https://api.github.com/repos/Codertocat/Hello-World/git/trees{/sha}',
     statuses_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/statuses/{sha}',
+      'https://api.github.com/repos/Codertocat/Hello-World/statuses/{sha}',
     languages_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/languages',
+      'https://api.github.com/repos/Codertocat/Hello-World/languages',
     stargazers_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/stargazers',
+      'https://api.github.com/repos/Codertocat/Hello-World/stargazers',
     contributors_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/contributors',
+      'https://api.github.com/repos/Codertocat/Hello-World/contributors',
     subscribers_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/subscribers',
+      'https://api.github.com/repos/Codertocat/Hello-World/subscribers',
     subscription_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/subscription',
+      'https://api.github.com/repos/Codertocat/Hello-World/subscription',
     commits_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/commits{/sha}',
+      'https://api.github.com/repos/Codertocat/Hello-World/commits{/sha}',
     git_commits_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/git/commits{/sha}',
+      'https://api.github.com/repos/Codertocat/Hello-World/git/commits{/sha}',
     comments_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/comments{/number}',
+      'https://api.github.com/repos/Codertocat/Hello-World/comments{/number}',
     issue_comment_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/issues/comments{/number}',
+      'https://api.github.com/repos/Codertocat/Hello-World/issues/comments{/number}',
     contents_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/contents/{+path}',
+      'https://api.github.com/repos/Codertocat/Hello-World/contents/{+path}',
     compare_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/compare/{base}...{head}',
-    merges_url: 'https://api.github.com/repos/Enterprise/Hello-World/merges',
+      'https://api.github.com/repos/Codertocat/Hello-World/compare/{base}...{head}',
+    merges_url: 'https://api.github.com/repos/Codertocat/Hello-World/merges',
     archive_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/{archive_format}{/ref}',
+      'https://api.github.com/repos/Codertocat/Hello-World/{archive_format}{/ref}',
     downloads_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/downloads',
+      'https://api.github.com/repos/Codertocat/Hello-World/downloads',
     issues_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/issues{/number}',
+      'https://api.github.com/repos/Codertocat/Hello-World/issues{/number}',
     pulls_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/pulls{/number}',
+      'https://api.github.com/repos/Codertocat/Hello-World/pulls{/number}',
     milestones_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/milestones{/number}',
+      'https://api.github.com/repos/Codertocat/Hello-World/milestones{/number}',
     notifications_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/notifications{?since,all,participating}',
+      'https://api.github.com/repos/Codertocat/Hello-World/notifications{?since,all,participating}',
     labels_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/labels{/name}',
+      'https://api.github.com/repos/Codertocat/Hello-World/labels{/name}',
     releases_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/releases{/id}',
+      'https://api.github.com/repos/Codertocat/Hello-World/releases{/id}',
     deployments_url:
-      'https://api.github.com/repos/Enterprise/Hello-World/deployments',
+      'https://api.github.com/repos/Codertocat/Hello-World/deployments',
     created_at: '2019-05-15T15:19:25Z',
     updated_at: '2019-05-15T15:19:27Z',
     pushed_at: '2019-05-15T15:20:13Z',
-    git_url: 'git://github.com/Enterprise/Hello-World.git',
-    ssh_url: 'git@github.com:Enterprise/Hello-World.git',
-    clone_url: 'https://github.com/Enterprise/Hello-World.git',
-    svn_url: 'https://github.com/Enterprise/Hello-World',
+    git_url: 'git://github.com/Codertocat/Hello-World.git',
+    ssh_url: 'git@github.com:Codertocat/Hello-World.git',
+    clone_url: 'https://github.com/Codertocat/Hello-World.git',
+    svn_url: 'https://github.com/Codertocat/Hello-World',
     homepage: null,
     size: 0,
     stargazers_count: 0,
@@ -192,22 +196,25 @@ export default {
     default_branch: 'master',
   },
   sender: {
-    login: 'Picard',
+    login: 'Codertocat',
     id: 21031067,
     node_id: 'MDQ6VXNlcjIxMDMxMDY3',
     avatar_url: 'https://avatars1.githubusercontent.com/u/21031067?v=4',
     gravatar_id: '',
-    url: 'https://api.github.com/users/Picard',
-    html_url: 'https://github.com/Picard',
-    followers_url: 'https://api.github.com/users/Picard/followers',
-    following_url: 'https://api.github.com/users/Picard/following{/other_user}',
-    gists_url: 'https://api.github.com/users/Picard/gists{/gist_id}',
-    starred_url: 'https://api.github.com/users/Picard/starred{/owner}{/repo}',
-    subscriptions_url: 'https://api.github.com/users/Picard/subscriptions',
-    organizations_url: 'https://api.github.com/users/Picard/orgs',
-    repos_url: 'https://api.github.com/users/Picard/repos',
-    events_url: 'https://api.github.com/users/Picard/events{/privacy}',
-    received_events_url: 'https://api.github.com/users/Picard/received_events',
+    url: 'https://api.github.com/users/Codertocat',
+    html_url: 'https://github.com/Codertocat',
+    followers_url: 'https://api.github.com/users/Codertocat/followers',
+    following_url:
+      'https://api.github.com/users/Codertocat/following{/other_user}',
+    gists_url: 'https://api.github.com/users/Codertocat/gists{/gist_id}',
+    starred_url:
+      'https://api.github.com/users/Codertocat/starred{/owner}{/repo}',
+    subscriptions_url: 'https://api.github.com/users/Codertocat/subscriptions',
+    organizations_url: 'https://api.github.com/users/Codertocat/orgs',
+    repos_url: 'https://api.github.com/users/Codertocat/repos',
+    events_url: 'https://api.github.com/users/Codertocat/events{/privacy}',
+    received_events_url:
+      'https://api.github.com/users/Codertocat/received_events',
     type: 'User',
     site_admin: false,
   },
