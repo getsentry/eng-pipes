66f80eb3f57ce91b7e55c9e1fb40d9ce27250d58
Billy Vong <billyvg@users.noreply.github.com>
2021-03-01 11:41:17 -0800

    feat(migrations): Add database for deploys (#143)

---
+++ migrations/20210301102340_add_deploys.ts
@@ -0,0 +1,23 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('deploys', (table) => {
+    table.bigInteger('id').primary();
+    table.bigInteger('user_id').notNullable();
+    table.string('app_name').notNullable();
+    table.string('user').notNullable();
+    table.string('ref').notNullable();
+    table.string('sha').notNullable();
+    table.string('previous_sha').notNullable();
+    table.string('status').notNullable();
+    table.string('environment').notNullable();
+    table.integer('duration');
+    table.timestamp('created_at');
+    table.timestamp('started_at');
+    table.timestamp('finished_at');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('deploys');
+}
