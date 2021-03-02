a0012ea6b099f0bbfa7c687ed8a209e3e6899e14
Billy Vong <billy@sentry.io>
2021-03-01 17:57:54 -0800

    wip tests

--- src/brain/pleaseDeployNotifier/index.test.ts
+++ src/brain/pleaseDeployNotifier/index.test.ts
@@ -4,6 +4,7 @@ jest.mock('@api/github/getClient');
 import merge from 'lodash.merge';
 
 import { createGitHubEvent } from '@test/utils/createGitHubEvent';
+import { createSlackEvent } from '@test/utils/createSlackEvent';
 
 import { buildServer } from '@/buildServer';
 import { REQUIRED_CHECK_CHANNEL, REQUIRED_CHECK_NAME } from '@/config';
@@ -12,6 +13,7 @@ import { getClient } from '@api/github/getClient';
 import { bolt } from '@api/slack';
 import { db } from '@utils/db';
 
+import * as actions from './actionViewUndeployedCommits';
 import { pleaseDeployNotifier } from '.';
 
 describe('pleaseDeployNotifier', function () {
@@ -20,6 +22,36 @@ describe('pleaseDeployNotifier', function () {
 
   beforeAll(async function () {
     await db.migrate.latest();
+    jest.spyOn(actions, 'actionViewUndeployedCommits');
+
+    octokit = await getClient('getsentry', 'getsentry');
+    octokit.repos.getCommit.mockImplementation(({ repo, ref }) => {
+      const defaultPayload = require('@test/payloads/github/commit').default;
+      if (repo === 'sentry') {
+        return {
+          data: merge({}, defaultPayload, {
+            commit: {
+              author: {
+                name: 'mars',
+                email: 'mars@sentry.io',
+                date: '2021-02-03T11:06:46Z',
+              },
+              committer: {
+                name: 'GitHub',
+                email: 'noreply@github.com',
+                date: '2021-02-03T11:06:46Z',
+              },
+              message:
+                'feat(ui): Change default period for fresh releases (#23572)\n' +
+                '\n' +
+                'The fresh releases (not older than one day) will have default statsPeriod on the release detail page set to 24 hours.',
+            },
+          }),
+        };
+      }
+
+      return { data: defaultPayload };
+    });
   });
 
   afterAll(async function () {
@@ -29,7 +61,6 @@ describe('pleaseDeployNotifier', function () {
   beforeEach(async function () {
     fastify = await buildServer(false);
     await pleaseDeployNotifier();
-    octokit = await getClient('getsentry', 'getsentry');
   });
 
   afterEach(async function () {
@@ -70,34 +101,6 @@ describe('pleaseDeployNotifier', function () {
   });
 
   it('notifies slack user when check run is successful', async function () {
-    octokit.repos.getCommit.mockImplementation(({ repo, ref }) => {
-      const defaultPayload = require('@test/payloads/github/commit').default;
-      if (repo === 'sentry') {
-        return {
-          data: merge({}, defaultPayload, {
-            commit: {
-              author: {
-                name: 'mars',
-                email: 'mars@sentry.io',
-                date: '2021-02-03T11:06:46Z',
-              },
-              committer: {
-                name: 'GitHub',
-                email: 'noreply@github.com',
-                date: '2021-02-03T11:06:46Z',
-              },
-              message:
-                'feat(ui): Change default period for fresh releases (#23572)\n' +
-                '\n' +
-                'The fresh releases (not older than one day) will have default statsPeriod on the release detail page set to 24 hours.',
-            },
-          }),
-        };
-      }
-
-      return { data: defaultPayload };
-    });
-
     await createGitHubEvent(fastify, 'check_run', {
       repository: {
         full_name: 'getsentry/getsentry',
@@ -241,5 +244,153 @@ describe('pleaseDeployNotifier', function () {
         status: 'undeployed',
       },
     });
+  });
+
+  it('has a button to view undeployed commits in a modal if there is a successful deploy in db', async function () {
+    await db('deploys').insert({
+      external_id: 1,
+      user_id: 1,
+      app_name: 'getsentry',
+      user: 'test@sentry.io',
+      ref: 'master',
+      sha: '999999',
+      previous_sha: '888888',
+      environment: 'production',
+      status: 'finished',
+    });
+    await createGitHubEvent(fastify, 'check_run', {
+      repository: {
+        full_name: 'getsentry/getsentry',
+      },
+      check_run: {
+        status: 'completed',
+        conclusion: 'success',
+        name: REQUIRED_CHECK_NAME,
+        head_sha: '6d225cb77225ac655d817a7551a26fff85090fe6',
+        output: {
+          title: 'All checks passed',
+          summary: 'All checks passed',
+          text:
+            '\n' +
+            '# Required Checks\n' +
+            '\n' +
+            'These are the jobs that must pass before this commit can be deployed. Try re-running a failed job in case it is flakey.\n' +
+            '\n' +
+            '## Status of required checks\n' +
+            '\n' +
+            '| Job | Conclusion |\n' +
+            '| --- | ---------- |\n' +
+            '| [webpack](https://github.com/getsentry/getsentry/runs/1821955151) | ✅  success |\n',
+          annotations_count: 0,
+          annotations_url:
+            'https://api.github.com/repos/getsentry/getsentry/check-runs/1821995033/annotations',
+        },
+      },
+    });
+    // @ts-ignore
+    expect(bolt.client.chat.postMessage.mock.calls[0][0].attachments)
+      .toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "blocks": Array [
+            Object {
+              "text": Object {
+                "text": "<https://github.com/getsentry/getsentry/commit/6d225cb77225ac655d817a7551a26fff85090fe6|*feat(ui): Change default period for fresh releases (#23572)*>",
+                "type": "mrkdwn",
+              },
+              "type": "section",
+            },
+            Object {
+              "text": Object {
+                "text": "The fresh releases (not older than one day) will have default statsPeriod on the release detail page set to 24 hours.",
+                "type": "mrkdwn",
+              },
+              "type": "section",
+            },
+            Object {
+              "elements": Array [
+                Object {
+                  "alt_text": "mars",
+                  "image_url": "https://avatars.githubusercontent.com/u/9060071?v=4",
+                  "type": "image",
+                },
+                Object {
+                  "text": "<https://github.com/matejminar|mars (matejminar)>",
+                  "type": "mrkdwn",
+                },
+              ],
+              "type": "context",
+            },
+            Object {
+              "elements": Array [
+                Object {
+                  "action_id": "freight-deploy",
+                  "style": "primary",
+                  "text": Object {
+                    "emoji": true,
+                    "text": "Deploy",
+                    "type": "plain_text",
+                  },
+                  "type": "button",
+                  "url": "https://freight.getsentry.net/deploy?app=getsentry",
+                  "value": "6d225cb77225ac655d817a7551a26fff85090fe6",
+                },
+                Object {
+                  "action_id": "view-undeployed-commits",
+                  "text": Object {
+                    "emoji": true,
+                    "text": "View Undeployed Commits",
+                    "type": "plain_text",
+                  },
+                  "type": "button",
+                  "value": "999999:6d225cb77225ac655d817a7551a26fff85090fe6",
+                },
+                Object {
+                  "action_id": "mute-slack-deploy",
+                  "confirm": Object {
+                    "confirm": Object {
+                      "text": "Mute",
+                      "type": "plain_text",
+                    },
+                    "deny": Object {
+                      "text": "Cancel",
+                      "type": "plain_text",
+                    },
+                    "text": Object {
+                      "text": "Are you sure you want to mute these deploy notifications? You can re-enable them by DM-ing me
+
+      \`\`\`
+      deploy notifications on
+      \`\`\`
+
+      Or you can visit the App's \\"Home\\" tab in Slack.
+      ",
+                      "type": "mrkdwn",
+                    },
+                    "title": Object {
+                      "text": "Mute deploy notifications?",
+                      "type": "plain_text",
+                    },
+                  },
+                  "style": "danger",
+                  "text": Object {
+                    "emoji": true,
+                    "text": "Mute",
+                    "type": "plain_text",
+                  },
+                  "type": "button",
+                  "value": "mute",
+                },
+              ],
+              "type": "actions",
+            },
+          ],
+          "color": "#C6BECF",
+        },
+      ]
+    `);
+
+    await createSlackEvent(fastify, 'block_actions', {});
+    expect(actions.actionViewUndeployedCommits).toHaveBeenCalled();
   });
 });
