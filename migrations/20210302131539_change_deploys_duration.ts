275cffa0a7c0ecd4b06f2cc2fe226372a55b4a8f
Billy Vong <billy@sentry.io>
2021-03-02 16:37:38 -0800

    feat(migration): Fix `deploys.duration` to be float instead of int

---
+++ migrations/20210302131539_change_deploys_duration.ts
@@ -0,0 +1,13 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('deploys', (table) => {
+    table.float('duration').alter();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('deploys', (table) => {
+    table.integer('duration').alter();
+  });
+}
