d5e46751ebd35ac85465e87ab6e769a9ee0c064c
Chad Whitacre <chadwhitacre@sentry.io>
2021-05-17 11:51:55 -0400

    Factor out WebhookRouter
    The purpose of this small refactor is to make it easier to understand
    what is going on in buildServer.ts. I wanted to do this the other day
    (with #186), but my tired brain failed to see that Fastify spells
    "response" as "reply" for some reason. 🙄

---
+++ src/webhooks/index.ts
@@ -0,0 +1,42 @@
+import path from 'path';
+
+import * as Sentry from '@sentry/node';
+
+/**
+ * Return a function that routes webhook POSTs to this or that module
+ * based on the service name.
+ */
+
+export function WebhookRouter(server) {
+  return async function (request, reply) {
+    const rootDir = __dirname;
+    let handler;
+
+    try {
+      const handlerPath = path.join(__dirname, request.params.service);
+
+      // Prevent directory traversals
+      if (!handlerPath.startsWith(rootDir)) {
+        throw new Error('Invalid service');
+      }
+
+      ({ handler } = require(handlerPath));
+      if (!handler) {
+        throw new Error('Invalid service');
+      }
+    } catch (err) {
+      console.error(err);
+      Sentry.captureException(err);
+      // @ts-ignore
+      return server.notFound(request, reply);
+    }
+
+    try {
+      return await handler(request, reply);
+    } catch (err) {
+      console.error(err);
+      Sentry.captureException(err);
+      return reply.code(400).send('Bad Request');
+    }
+  };
+}
