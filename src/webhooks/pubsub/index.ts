3e7d980200fd89196380c66b87827c43f3505793
Burak Yigit Kaya <ben@byk.im>
2021-06-23 01:06:40 +0300

    tighten up code, fix multi-channel notification bug

--- src/webhooks/pubsub/index.ts
+++ src/webhooks/pubsub/index.ts
@@ -1,3 +1,5 @@
+import { Octokit } from '@octokit/rest';
+
 import {
   getLabelsTable,
   TEAM_LABEL_PREFIX,
@@ -8,13 +10,22 @@ import { getClient } from '@api/github/getClient';
 import { bolt } from '@api/slack';
 
 const DEFAULT_REPOS = [SENTRY_REPO];
-const MAX_TRIAGE_TIME = 4 * DAY_IN_MS;
+const MAX_TRIAGE_TIME = 0.5 * DAY_IN_MS;
 const GH_API_PER_PAGE = 100;
+const DEFAULT_TEAM_LABEL = 'Team: Open Source';
 
 type PubSubPayload = {
   name: string;
   slo?: number;
   repos?: string[];
+};
+
+type IssueSLOInfo = {
+  url: string;
+  number: number;
+  title: string;
+  teamLabel: string;
+  overSLO: boolean;
 };
 
 export const opts = {
@@ -37,16 +48,53 @@ export const opts = {
   },
 };
 
+function getIssueTeamLabel(issue: {
+  labels: {
+    name: string;
+  }[];
+}) {
+  return (
+    issue.labels.find((label) => label.name.startsWith(TEAM_LABEL_PREFIX))
+      ?.name || DEFAULT_TEAM_LABEL
+  );
+}
+
+async function getRoutingTimestamp(
+  octokit: Octokit,
+  repo: string,
+  issue_number: number
+) {
+  const { data } = await octokit.issues.listEvents({
+    owner: OWNER,
+    repo,
+    issue_number,
+    per_page: GH_API_PER_PAGE,
+  });
+
+  const untriagedLabelEvents = data.filter(
+    (event) =>
+      // @ts-ignore - We _know_ a `label` property exists on `labeled` events
+      event.event === 'labeled' && event.label.name === UNTRIAGED_LABEL
+  );
+  const lastUntriagedLabelEvent =
+    untriagedLabelEvents[untriagedLabelEvents.length - 1];
+  return Date.parse(lastUntriagedLabelEvent.created_at);
+}
+
 export const handler = async (request, reply) => {
   const payload: PubSubPayload = JSON.parse(
     Buffer.from(request.body.message.data, 'base64').toString().trim()
   );
 
+  // This is to make this endpoint accept different payloads and actions
+  // in the future. Ideally, we'd then split out all different event
+  // handlers into dedicated modules for clarity and isolation
   if (payload.name !== 'stale-triage-notifier') {
     reply.code(400);
     return reply.send();
   }
 
+  // Respond early to not block the webhook sender
   reply.code(204);
   reply.send();
 
@@ -55,81 +103,66 @@ export const handler = async (request, reply) => {
   const SLO = payload.slo || MAX_TRIAGE_TIME;
   const now = Date.now();
 
-  const issuesOverSLO = (
-    await Promise.all(
-      repos.map(async (repo) => {
-        const untriagedIssues = (
-          await octokit.issues.listForRepo({
-            owner: OWNER,
-            repo,
-            state: 'open',
-            labels: UNTRIAGED_LABEL,
-            per_page: GH_API_PER_PAGE,
-          })
-        ).data;
+  // 1. Get all open & untriaged issues
+  // 2. Get all the events for each of the remaining issues
+  // 3. Find the latest `label` event for the UNTRIAGED_LABEL
+  // 4. Get the time elapsed since that latest labeling event above
+  // 5. Filter out the issues which were labeled more than MAX_TRIAGE_TIME ago
+  const getIssueSLOInfoForRepo = async (
+    repo: string
+  ): Promise<IssueSLOInfo[]> => {
+    const { data: untriagedIssues } = await octokit.issues.listForRepo({
+      owner: OWNER,
+      repo,
+      state: 'open',
+      labels: UNTRIAGED_LABEL,
+      per_page: GH_API_PER_PAGE,
+    });
 
-        return Promise.all(
-          untriagedIssues.map(async (issue) => {
-            const teamLabel = issue.labels.find((label) =>
-              label.name.startsWith(TEAM_LABEL_PREFIX)
-            )?.name;
+    const issuesWithSLOInfo = untriagedIssues.map(async (issue) => ({
+      url: issue.html_url,
+      number: issue.number,
+      title: issue.title,
+      teamLabel: getIssueTeamLabel(issue),
+      // TODO(byk): Make this business days (at least weekend-aware)
+      overSLO:
+        now - (await getRoutingTimestamp(octokit, repo, issue.number)) >= SLO,
+    }));
 
-            if (!teamLabel) {
-              // This and the `flat(2)` below are to workaround
-              // https://github.com/microsoft/TypeScript/issues/16069#issuecomment-565658443
-              return [];
-            }
+    return Promise.all(issuesWithSLOInfo);
+  };
 
-            const { data } = await octokit.issues.listEvents({
-              owner: OWNER,
-              repo,
-              issue_number: issue.number,
-              per_page: GH_API_PER_PAGE,
-            });
+  const issuesOverSLO = (await Promise.all(repos.map(getIssueSLOInfoForRepo)))
+    .flat()
+    .filter((data) => data.overSLO);
 
-            const untriagedLabelEvents = data.filter(
-              (event) =>
-                event.event === 'labeled' &&
-                // @ts-ignore - We _know_ there's a `label` prop for `labeled` events
-                event.label.name === UNTRIAGED_LABEL
-            );
-            const lastUntriagedLabelEvent =
-              untriagedLabelEvents[untriagedLabelEvents.length - 1];
-            const labelTime = Date.parse(lastUntriagedLabelEvent.created_at);
-
-            return {
-              issue,
-              teamLabel,
-              labelTime,
-            };
-          })
-        );
-      })
-    )
-  )
-    .flat(2)
-    // TODO(byk): Make this business days (at least weekend-aware)
-    .filter((data) => now - data.labelTime >= SLO);
-
+  // Get an N-to-N mapping of `Team: *` labels to Slack Channels
   const teamsToNotify = new Set(
     issuesOverSLO.map((data) => data.teamLabel)
   ) as Set<string>;
-  const notificationChannels: Record<string, string> = Object.fromEntries(
-    (
-      await getLabelsTable()
-        .select('label_name', 'channel_id')
-        .whereIn('label_name', Array.from(teamsToNotify))
-    ).map((row) => [row.label_name, row.channel_id])
-  );
+  const notificationChannels: Record<string, string[]> = (
+    await getLabelsTable()
+      .select('label_name', 'channel_id')
+      .whereIn('label_name', Array.from(teamsToNotify))
+  ).reduce((res, { label_name, channel_id }) => {
+    const channels = res[label_name] || [];
+    channels.push(channel_id);
+    res[label_name] = channels;
+    return res;
+  }, {});
 
-  await Promise.all(
-    issuesOverSLO.map(async ({ issue, teamLabel }) =>
-      bolt.client.chat.postMessage({
-        text: `⚠ Issue over triage SLO: <${issue.html_url}|#${issue.number} ${issue.title}>`,
-        channel: notificationChannels[teamLabel],
-      })
-    )
+  // Notify all channels associated with the relevant `Team: *` label per issue
+  const notifications = issuesOverSLO.flatMap(
+    ({ url, number, title, teamLabel }) =>
+      notificationChannels[teamLabel].map((channel) =>
+        bolt.client.chat.postMessage({
+          channel,
+          text: `⚠ Issue over triage SLO: <${url}|#${number} ${title}>`,
+        })
+      )
   );
+  // Do all this in parallel and wait till all finish
+  await Promise.all(notifications);
 };
 
 // Test command for `sentry-docs` repo:
