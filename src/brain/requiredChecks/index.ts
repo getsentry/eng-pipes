c37ae8d96d736e0a75a0e86df7b828e2da204e99
Billy Vong <billy@sentry.io>
2021-06-23 11:09:13 -0700

    add loading/error/success messages in a thread

--- src/brain/requiredChecks/index.ts
+++ src/brain/requiredChecks/index.ts
@@ -465,72 +465,92 @@ export async function requiredChecks() {
     });
 
     // TODO: Do we need to check that user has permissions?
+    const { originalMessage, ...commitData } = JSON.parse(
+      view.private_metadata
+    );
+
+    // Notify the user in the original message thread that we are attempting to revert
+    const loadingMessage = await client.chat.postMessage({
+      channel: originalMessage.channel,
+      thread_ts: originalMessage.message.ts,
+      text: `<@${body.user.id}>, :sentry-loading: we are attempting to revert the commit... :sentry-loading:`,
+    });
+
     try {
-      const { originalMessage, ...commitData } = JSON.parse(
-        view.private_metadata
-      );
-
-      // Notify the user in the original message thread that we are attempting to revert
-      const loadingMessage = await client.chat.postMessage({
-        channel: originalMessage.channel,
-        thread_ts: originalMessage.message.ts,
-        text: `:sentry-loading: <@${body.user.id}>, we are attempting to revert the commit...`,
-      });
-
       await revertCommit({
         ...commitData,
         name: `${body.user.name} via Slack${user ? ` <${user.email}>` : ''}`,
       });
-
-      // Update the loading message
-      await client.chat.update({
-        channel: originalMessage.channel,
-        ts: `${loadingMessage.ts}`,
-        text: `:successkid: <@${body.user.id}>, the commit has been reverted.`,
-      });
-
-      // We semi-assume there is only one attachments block as we will not
-      // update any other attachments.
-      //
-      // Ignore `id` and `fallback` properties
-      const {
-        id: _id,
-        fallback: _fallback,
-        ...attachment
-      } = originalMessage.message.attachments.find(
-        ({ id }) => String(id) === String(originalMessage.attachmentId)
-      );
-
-      // Remove the actions block where the Revert button is as it will be the only
-      // element there
-      const updatedBlocks = attachment.blocks.filter(
-        ({ block_id }) => block_id !== originalMessage.revertBlockId
-      );
-
-      // Find original message to remove the Revert button
-      // @ts-ignore - `text` is not actually required
-      await client.chat.update({
-        channel: originalMessage.channel,
-        ts: originalMessage.message.ts,
-        attachments: [
-          {
-            ...attachment,
-            blocks: [
-              ...updatedBlocks,
-              {
-                type: 'section',
-                text: {
-                  type: 'mrkdwn',
-                  text: `**Update** Commit reverted by <@${body.user.id}>`,
-                },
-              },
-            ],
-          },
-        ],
-      });
     } catch (err) {
+      // Update the loading message with error message
+      await Promise.all([
+        client.chat.delete({
+          channel: originalMessage.channel,
+          ts: `${loadingMessage.ts}`,
+        }),
+        client.chat.postMessage({
+          channel: originalMessage.channel,
+          thread_ts: `${loadingMessage.ts}`,
+          text: `<@${body.user.id}>, there was an error reverting the commit.`,
+        }),
+      ]);
+
       console.error(err);
       Sentry.captureException(err);
+      return;
     }
+
+    // Update the loading message with success message
+    await Promise.all([
+      client.chat.delete({
+        channel: originalMessage.channel,
+        ts: `${loadingMessage.ts}`,
+      }),
+      client.chat.postMessage({
+        channel: originalMessage.channel,
+        thread_ts: `${loadingMessage.ts}`,
+        text: `<@${body.user.id}>, the commit has been reverted :successkid:`,
+      }),
+    ]);
+
+    // We semi-assume there is only one attachments block as we will not
+    // update any other attachments.
+    //
+    // Ignore `id` and `fallback` properties
+    const {
+      id: _id,
+      fallback: _fallback,
+      ...attachment
+    } = originalMessage.message.attachments.find(
+      ({ id }) => String(id) === String(originalMessage.attachmentId)
+    );
+
+    // Remove the actions block where the Revert button is as it will be the only
+    // element there
+    const updatedBlocks = attachment.blocks.filter(
+      ({ block_id }) => block_id !== originalMessage.revertBlockId
+    );
+
+    // Find original message to remove the Revert button
+    // @ts-ignore - `text` is not actually required
+    await client.chat.update({
+      channel: originalMessage.channel,
+      ts: originalMessage.message.ts,
+      attachments: [
+        {
+          ...attachment,
+          blocks: [
+            ...updatedBlocks,
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: `*Update* Commit reverted by <@${body.user.id}>`,
+              },
+            },
+          ],
+        },
+      ],
+    });
   });
 }
