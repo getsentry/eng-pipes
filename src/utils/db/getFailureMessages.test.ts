d465e052ad2cb2f14eb89351223dd16e5067fdb8
Billy Vong <billyvg@users.noreply.github.com>
2021-09-22 11:45:05 -0700

    feat: Fix required checks to respect commit timestamp (#236)
    This fixes required checks to correctly mark only older failing builds (based on commit timestamp) as fixed in Slack.
    
    If we have two builds:
    * `A` - which will pass CI and is merged before `B`
    * `B` - which is fail CI and is merged after `A`
    
    When `B` fails it will send a Slack message saying it is failing. Assuming `A` does not complete before `B` fails, when `A` finally finishes, it will *incorrectly* mark `B` as being fixed. When a build finishes, we need to compare the commits of 1) the build that finished with 2) the builds that failed and make sure that we only mark builds that have started after the build that finished.
    
    Closes https://github.com/getsentry/eng-pipes/issues/234

--- src/utils/db/getFailureMessages.test.ts
+++ src/utils/db/getFailureMessages.test.ts
@@ -1,12 +1,18 @@
 import { SlackMessage } from '@/config/slackMessage';
+import { getClient } from '@api/github/getClient';
 import { db } from '@utils/db';
 import { saveSlackMessage } from '@utils/db/saveSlackMessage';
 
 import { getFailureMessages } from './getFailureMessages';
 
 describe('getFailureMessages', function () {
+  let octokit;
   beforeAll(async function () {
     await db.migrate.latest();
+  });
+
+  beforeEach(async function () {
+    octokit = await getClient('getsentry');
   });
 
   afterAll(async function () {
@@ -15,6 +21,7 @@ describe('getFailureMessages', function () {
 
   afterEach(async function () {
     await db('slack_messages').delete();
+    octokit.repos.compareCommits.mockClear();
   });
 
   it('initially is not failing', async function () {
@@ -71,5 +78,62 @@ describe('getFailureMessages', function () {
       }
     );
     expect(await getFailureMessages(null)).toHaveLength(1);
+  });
+
+  it('returns messages only for older (or identical) commits', async function () {
+    await saveSlackMessage(
+      SlackMessage.REQUIRED_CHECK,
+      {
+        refId: 'aaa',
+        channel: 'channel',
+        ts: '123.123',
+      },
+      {
+        status: 'failure',
+        failed_at: new Date(),
+      }
+    );
+
+    octokit.repos.compareCommits.mockImplementation(() => ({
+      status: 200,
+      data: {
+        status: 'behind',
+      },
+    }));
+
+    expect(await getFailureMessages(null, 'bbb')).toHaveLength(1);
+
+    octokit.repos.compareCommits.mockImplementation(() => ({
+      status: 200,
+      data: {
+        status: 'identical',
+      },
+    }));
+
+    expect(await getFailureMessages(null, 'bbb')).toHaveLength(1);
+  });
+
+  it('does not return messages for commits newer input commit sha', async function () {
+    await saveSlackMessage(
+      SlackMessage.REQUIRED_CHECK,
+      {
+        refId: 'aaa',
+        channel: 'channel',
+        ts: '123.123',
+      },
+      {
+        status: 'failure',
+        failed_at: new Date(),
+      }
+    );
+
+    octokit.repos.compareCommits.mockImplementation(() => ({
+      status: 200,
+      data: {
+        status: 'ahead',
+      },
+    }));
+
+    expect(await getFailureMessages(null, 'bbb')).toHaveLength(0);
   });
 });
