f141205c39a311568eff59511240b79940d33f9e
Billy Vong <billyvg@users.noreply.github.com>
2021-11-15 09:26:57 -0500

    test(brain): Add tests for #252 (#253)
    Wanted to get the hotfix (#252) out, but added some tests here.

--- src/brain/pleaseDeployNotifier/index.test.ts
+++ src/brain/pleaseDeployNotifier/index.test.ts
@@ -1081,6 +1081,219 @@ Remove "always()" from GHA workflows`,
     });
   });
 
+  it('does not allow frontend deploy if head commit is a backend change', async function () {
+    await db('deploys').insert({
+      external_id: 1,
+      user_id: 1,
+      app_name: 'getsentry',
+      user: 'test@sentry.io',
+      ref: 'master',
+      sha: '333333',
+      previous_sha: '888888',
+      environment: 'production',
+      status: 'finished',
+    });
+
+    octokit.repos.getCommit.mockImplementation(({ repo, ref }) => {
+      const defaultPayload = require('@test/payloads/github/commit').default;
+      return {
+        data: merge({}, defaultPayload, {
+          commit: {
+            author: {
+              name: 'mars',
+              email: 'mars@sentry.io',
+              date: '2021-02-03T11:06:46Z',
+            },
+            committer: {
+              name: 'GitHub',
+              email: 'noreply@github.com',
+              date: '2021-02-03T11:06:46Z',
+            },
+            message: `build(ci): Remove if: always() conditions from workflows (#29414)
+
+Remove "always()" from GHA workflows`,
+          },
+        }),
+      };
+    });
+
+    octokit.checks.listForRef.mockImplementation(({ ref, repo }) => {
+      if (ref === '333333') {
+        return {
+          data: {
+            check_runs: [
+              {
+                name: 'only frontend changes',
+                conclusion: 'success',
+              },
+            ],
+          },
+        };
+      } else if (ref === '6d225cb77225ac655d817a7551a26fff85090fe6') {
+        return {
+          data: {
+            check_runs: [
+              {
+                name: 'only backend changes',
+                conclusion: 'success',
+              },
+            ],
+          },
+        };
+      } else return null;
+    });
+
+    await createGitHubEvent(fastify, 'check_run', {
+      repository: {
+        full_name: 'getsentry/getsentry',
+      },
+      check_run: {
+        status: 'completed',
+        conclusion: 'success',
+        name: REQUIRED_CHECK_NAME,
+        head_sha: '6d225cb77225ac655d817a7551a26fff85090fe6',
+        output: {
+          title: 'All checks passed',
+          summary: 'All checks passed',
+          text:
+            '\n' +
+            '# Required Checks\n' +
+            '\n' +
+            'These are the jobs that must pass before this commit can be deployed. Try re-running a failed job in case it is flakey.\n' +
+            '\n' +
+            '## Status of required checks\n' +
+            '\n' +
+            '| Job | Conclusion |\n' +
+            '| --- | ---------- |\n' +
+            '| [webpack](https://github.com/getsentry/getsentry/runs/1821955151) | ✅  success |\n',
+          annotations_count: 0,
+          annotations_url:
+            'https://api.github.com/repos/getsentry/getsentry/check-runs/1821995033/annotations',
+        },
+      },
+    });
+
+    // First message
+    expect(bolt.client.chat.postMessage).toHaveBeenCalledWith(
+      expect.objectContaining({
+        channel: 'U789123',
+        text:
+          'Your commit getsentry@<https://github.com/getsentry/getsentry/commits/6d225cb77225ac655d817a7551a26fff85090fe6|6d225cb> is ready to deploy',
+      })
+    );
+
+    // @ts-ignore
+    expect(bolt.client.chat.postMessage.mock.calls[0][0].attachments)
+      .toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "blocks": Array [
+            Object {
+              "text": Object {
+                "text": "<https://github.com/getsentry/getsentry/commit/6d225cb77225ac655d817a7551a26fff85090fe6|*build(ci): Remove if: always() conditions from workflows (#29414)*>",
+                "type": "mrkdwn",
+              },
+              "type": "section",
+            },
+            Object {
+              "text": Object {
+                "text": "Remove \\"always()\\" from GHA workflows",
+                "type": "mrkdwn",
+              },
+              "type": "section",
+            },
+            Object {
+              "elements": Array [
+                Object {
+                  "alt_text": "mars",
+                  "image_url": "https://avatars.githubusercontent.com/u/9060071?v=4",
+                  "type": "image",
+                },
+                Object {
+                  "text": "<https://github.com/matejminar|mars (matejminar)>",
+                  "type": "mrkdwn",
+                },
+              ],
+              "type": "context",
+            },
+            Object {
+              "elements": Array [
+                Object {
+                  "action_id": "freight-deploy: getsentry",
+                  "style": "primary",
+                  "text": Object {
+                    "emoji": true,
+                    "text": "Deploy",
+                    "type": "plain_text",
+                  },
+                  "type": "button",
+                  "url": "https://freight.getsentry.net/deploy?app=getsentry",
+                  "value": "6d225cb77225ac655d817a7551a26fff85090fe6",
+                },
+                Object {
+                  "action_id": "view-undeployed-commits-",
+                  "text": Object {
+                    "emoji": true,
+                    "text": "View Undeployed Commits",
+                    "type": "plain_text",
+                  },
+                  "type": "button",
+                  "value": "6d225cb77225ac655d817a7551a26fff85090fe6",
+                },
+                Object {
+                  "action_id": "mute-slack-deploy",
+                  "confirm": Object {
+                    "confirm": Object {
+                      "text": "Mute",
+                      "type": "plain_text",
+                    },
+                    "deny": Object {
+                      "text": "Cancel",
+                      "type": "plain_text",
+                    },
+                    "text": Object {
+                      "text": "Are you sure you want to mute these deploy notifications? You can re-enable them by DM-ing me
+
+      \`\`\`
+      deploy notifications on
+      \`\`\`
+
+      Or you can visit the App's \\"Home\\" tab in Slack.
+      ",
+                      "type": "mrkdwn",
+                    },
+                    "title": Object {
+                      "text": "Mute deploy notifications?",
+                      "type": "plain_text",
+                    },
+                  },
+                  "style": "danger",
+                  "text": Object {
+                    "emoji": true,
+                    "text": "Mute",
+                    "type": "plain_text",
+                  },
+                  "type": "button",
+                  "value": "mute",
+                },
+              ],
+              "type": "actions",
+            },
+          ],
+          "color": "#E7E1EC",
+        },
+      ]
+    `);
+
+    expect(await db('slack_messages').first('*')).toMatchObject({
+      refId: '6d225cb77225ac655d817a7551a26fff85090fe6',
+      channel: 'channel_id',
+      ts: '1234123.123',
+      context: {
+        status: 'undeployed',
+      },
+    });
+  });
   it('does not allow frontend deploy if there are backend changes', async function () {
     // Make sure it checks that all commits are frontend only
     await db('deploys').insert({
