d5bed279ad74a1bcd2c87186cabbf1efc30767cc
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-08-10 11:32:36 +0200

    feat(brain): Add react testing library brain item

---
+++ src/brain/reactTestingLibrary/index.ts
@@ -0,0 +1,47 @@
+import { bolt } from '@api/slack';
+import { wrapHandler } from '@utils/wrapHandler';
+
+import { getProgress } from './getProgress';
+
+export function reactTestingLibrary() {
+  bolt.event(
+    'app_mention',
+    wrapHandler('rtl', async ({ event, say, client }) => {
+      if (!event.text.includes('rtl')) {
+        return;
+      }
+
+      const { message } = await say({
+        text: ':sentry-loading: fetching status ...',
+        blocks: [
+          {
+            type: 'context',
+            elements: [
+              {
+                type: 'mrkdwn',
+                text: ':sentry-loading: fetching status ...',
+              },
+            ],
+          },
+        ],
+      });
+
+      const { remainingFiles, progress } = await getProgress();
+
+      await client.chat.update({
+        channel: String(message.channel),
+        ts: String(message.ts),
+        text: `RTL progress: ${progress}% completed, ${remainingFiles} files remaining`,
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `:rtl: progress: *${progress}%* completed, *${remainingFiles}* files remaining`,
+            },
+          },
+        ],
+      });
+    })
+  );
+}
