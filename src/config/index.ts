061858065f1306d889abfc93c69c3904205ec8fc
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2022-10-19 13:32:46 -0700

    Making owner and repos configurable

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,9 +7,9 @@ export const DEFAULT_PORT = 3000;
 
 export const DAY_IN_MS = 1000 * 60 * 60 * 24;
 
-export const OWNER = 'getsentry';
-export const SENTRY_REPO = 'sentry';
-export const GETSENTRY_REPO = 'getsentry';
+export const OWNER = process.env.OWNER || 'getsentry';
+export const SENTRY_REPO = process.env.SENTRY_REPO || 'sentry';
+export const GETSENTRY_REPO = process.env.GETSENTRY_REPO || 'getsentry';
 export const GETSENTRY_BOT_ID = 10587625;
 
 // The name of the GitHub Check that is created in getsentry to aggregate "required" jobs
