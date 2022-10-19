30821934d854b4596780710ca7b97910b7139265
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2022-10-19 16:08:54 -0700

    Adding gocd webhook

---
+++ src/webhooks/gocd/gocd.test.ts
@@ -0,0 +1,34 @@
+import gocdagentpayload from '@test/payloads/gocd/gocd-agent.json';
+import gocdstagepayload from '@test/payloads/gocd/gocd-stage.json';
+
+import { buildServer } from '@/buildServer';
+
+describe('freight webhook', function () {
+  let fastify;
+  beforeEach(async function () {
+    fastify = await buildServer(false);
+  });
+
+  it('correctly inserts gocd webhook when stage starts', async function () {
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/metrics/gocd/webhook',
+      payload: gocdstagepayload,
+    });
+
+    expect(response.statusCode).toBe(200);
+
+    // TODO (mattgauntseo-sentry): Check data is stored correctly in
+    // database
+  });
+
+  it('does nothing for agent updates', async function () {
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/metrics/gocd/webhook',
+      payload: gocdagentpayload,
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+});
