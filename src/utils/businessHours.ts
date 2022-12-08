8484f7b1821d873ebd7289df2dcfcb3ff544de70
Hubert Deng <hubert.deng@sentry.io>
2022-12-07 16:44:01 -0800

    Add undefined/null check for getOfficesForTeam (#364)
    * add undefined/null check for getOfficesForTeam
    
    * add undefined/null office check in calculateTimeToRespondBy
    
    * Reword null test case
    
    Co-authored-by: Matt Gaunt-Seo @ Sentry.io <112419115+mattgauntseo-sentry@users.noreply.github.com>
    
    Co-authored-by: Matt Gaunt-Seo @ Sentry.io <112419115+mattgauntseo-sentry@users.noreply.github.com>

--- src/utils/businessHours.ts
+++ src/utils/businessHours.ts
@@ -40,7 +40,7 @@ export async function calculateTimeToRespondBy(numDays, timestamp, team) {
     let shouldSkipDate = false;
     offices.forEach((office) => {
       if (
-        HOLIDAY_CONFIG[office].dates.includes(
+        HOLIDAY_CONFIG[office]?.dates.includes(
           // slicing the string here since we only care about YYYY/MM/DD
           dateObj.toISOString().slice(0, 10)
         )
@@ -79,10 +79,7 @@ export async function calculateSLOViolationTriage(
   return null;
 }
 
-export async function calculateSLOViolationRoute(
-  target_name,
-  timestamp
-) {
+export async function calculateSLOViolationRoute(target_name, timestamp) {
   if (target_name === UNROUTED_LABEL) {
     return calculateTimeToRespondBy(MAX_ROUTE_DAYS, timestamp, 'Team: Support');
   }
@@ -107,6 +104,9 @@ export async function cacheOfficesForTeam(team) {
 }
 
 export async function getOfficesForTeam(team) {
+  if (!team) {
+    return [];
+  }
   if (!officesCache[team]) {
     await cacheOfficesForTeam(team);
   }
