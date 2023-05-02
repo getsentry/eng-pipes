eec6130065f70a455a301e1cf0cdb4845158ca4b
Chad Whitacre <chadwhitacre@sentry.io>
2023-05-02 18:01:24 -0400

    Use it.each

--- src/utils/slugizeProductArea.test.ts
+++ src/utils/slugizeProductArea.test.ts
@@ -2,17 +2,16 @@ import { slugizeProductArea } from './slugizeProductArea';
 
 describe('slugizeProductArea', function () {
   describe('slugize tests', function () {
-    it('behaves as expected', async function () {
-      const cases = [
-        ['MOST of ALL', 'most-of-all'],
-        ['Cheese & Bread', 'cheese-bread'],
-        ['otherwise - notherwise', 'otherwise-notherwise'],
-        ['over - & - yonder', 'over-yonder'],
-        ["other's druthers", 'others-druthers'],
-      ];
-      cases.forEach((x) => {
-        expect(slugizeProductArea(x[0])).toEqual(x[1]);
-      });
+    it.each([
+      ['MOST of ALL', 'most-of-all'],
+      ['Cheese & Bread', 'cheese-bread'],
+      ['otherwise - notherwise', 'otherwise-notherwise'],
+      ['over - & - yonder', 'over-yonder'],
+      ["other's druthers", 'others-druthers'],
+    ])("converts '%s' to '%s'", (input, output) => {
+      expect(slugizeProductArea(input)).toEqual(output);
+    });
+    it('handles errors', () => {
       expect(() => {
         slugizeProductArea('other^s druthers');
       }).toThrow('Bad slug: other^s-druthers');
