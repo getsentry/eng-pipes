ef2814bd5bee3981434e763100a5538fe3a720a2
Hubert Deng <hubertdeng123@gmail.com>
2023-06-21 11:12:25 -0700

    fix failing test

--- src/brain/issueLabelHandler/index.test.ts
+++ src/brain/issueLabelHandler/index.test.ts
@@ -518,6 +518,33 @@ describe('issueLabelHandler', function () {
       ]);
       expect(modifyProjectIssueFieldSpy).toHaveBeenCalled();
     });
+
+    it('uses different timestamp for eu office', async function () {
+      await createIssue('sentry-docs');
+      const command = {
+        channel_id: 'CHNLIDRND1',
+        text: 'Test vie',
+      };
+      const client = {
+        conversations: {
+          info: jest
+            .fn()
+            .mockReturnValue({ channel: { name: 'test', is_member: true } }),
+          join: jest.fn(),
+        },
+      };
+      await slackHandler({ command, ack, say, respond, client });
+      jest
+        .spyOn(businessHourFunctions, 'calculateSLOViolationTriage')
+        .mockReturnValue('2022-12-21T13:00:00.000Z');
+      await addLabel('Product Area: Test', 'sentry-docs');
+      expectUntriaged();
+      expectRouted();
+      expect(octokit.issues._comments).toEqual([
+        'Assigning to @getsentry/support for [routing](https://open.sentry.io/triage/#2-route), due by **<time datetime=2022-12-20T00:00:00.000Z>Monday, December 19th at 4:00 pm</time> (sfo)**. ⏲️',
+        'Routing to @getsentry/product-owners-test for [triage](https://develop.sentry.dev/processing-tickets/#3-triage), due by **<time datetime=2022-12-21T13:00:00.000Z>Wednesday, December 21st at 14:00</time> (vie)**. ⏲️',
+      ]);
+    });
   });
 
   describe('followups test cases', function () {
