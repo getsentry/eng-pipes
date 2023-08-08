c43437ff2bf9c4127745f4b795ee77623d4065d2
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-08-08 09:51:59 -0700

    Adding logic to properly handle gocd events without a group defined

--- src/api/gocdevents/index.test.ts
+++ src/api/gocdevents/index.test.ts
@@ -20,11 +20,36 @@ describe('gocdevents', function () {
     gocdevents.on('stage', stageSpy);
     gocdevents.on('agent', agentSpy);
 
-    gocdevents.emit('stage', {});
-    gocdevents.emit('agent', {});
+    gocdevents.emit('stage', {
+      data: {
+        pipeline: { name: 'test-name', group: 'test-group' },
+      },
+    });
+    gocdevents.emit('agent', {
+      data: {
+        pipeline: { name: 'test-name', group: 'test-group' },
+      },
+    });
 
     expect(starSpy).toHaveBeenCalledTimes(2);
     expect(stageSpy).toHaveBeenCalledTimes(1);
+    expect(agentSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('do not emit stage when no group is provided', () => {
+    const starSpy = jest.fn();
+    const stageSpy = jest.fn();
+    const agentSpy = jest.fn();
+
+    gocdevents.on('*', starSpy);
+    gocdevents.on('stage', stageSpy);
+    gocdevents.on('agent', agentSpy);
+
+    gocdevents.emit('stage', { data: { pipeline: { name: 'test-name' } } });
+    gocdevents.emit('agent', { data: { pipeline: { name: 'test-name' } } });
+
+    expect(starSpy).toHaveBeenCalledTimes(1);
+    expect(stageSpy).toHaveBeenCalledTimes(0);
     expect(agentSpy).toHaveBeenCalledTimes(1);
   });
 });
