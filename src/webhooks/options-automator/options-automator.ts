054141cdd283782872b4dfe1c4cb0a37f7bb0bc2
kneeyo1 <neo.huang@sentry.io>
2023-08-11 10:45:30 -0500

    updated testcases

--- src/webhooks/options-automator/options-automator.ts
+++ src/webhooks/options-automator/options-automator.ts
@@ -19,8 +19,8 @@ export async function handler(
 export async function messageSlack(message: OptionsAutomatorResponse) {
     const successBlock: KnownBlock[] = [
         slackblocks.header(slackblocks.plaintext(message.region
-            ? `✅ Successfully Updated Options: ✅ in ${message.region}:`
-            : '')),
+            ? `✅ Successfully Updated Options in ${message.region}: ✅`
+            : '✅ Successfully Updated Options ✅')),
         ...(message.updated_options.length > 0
             ? [
                 slackblocks.divider(),
@@ -62,8 +62,8 @@ export async function messageSlack(message: OptionsAutomatorResponse) {
     
     const failedBlock: KnownBlock[] = [
         slackblocks.header(slackblocks.plaintext(message.region
-            ? `❌ FAILED TO UPDATE: ❌ in ${message.region}:`
-            : '')),
+            ? `❌ FAILED TO UPDATE in ${message.region}: ❌`
+            : '❌ FAILED TO UPDATE ❌')),
         ...(message.drifted_options.length > 0
             ? [
                 slackblocks.divider(),
