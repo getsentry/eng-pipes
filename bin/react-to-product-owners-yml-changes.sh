1a032b6f797c85c40af0b6b5338f09c5849ef9c5
Hubert Deng <hubert.deng@sentry.io>
2023-08-16 10:32:52 -0700

    git add product-owners.yml (#586)

--- bin/react-to-product-owners-yml-changes.sh
+++ bin/react-to-product-owners-yml-changes.sh
@@ -11,9 +11,7 @@ message="Sync with product-owners.yml in security-as-code@${sha:0:8}"
 git config user.email "getsantry[bot]@users.noreply.github.com"
 git config user.name "getsantry[bot]"
 git checkout -b "$branch"
-git add .github/labels.yml
-git add .github/ISSUE_TEMPLATE/bug.yml
-git add .github/ISSUE_TEMPLATE/feature.yml
+git add product-owners.yml
 git commit -n -m "$message" || exit 0
 git push --set-upstream origin "$branch"
 gh pr create --title "meta(routing) $message" --body="Syncing with [``product-owners.yml``](https://github.com/getsentry/security-as-code/blob/$sha/rbac/lib/product-owners.yml) ([docs](https://www.notion.so/473791bae5bf43399d46093050b77bf0))."
