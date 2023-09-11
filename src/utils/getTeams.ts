57a672e16da2580c8747ed4c0aaa2142bfb6a869
Hubert Deng <hubert.deng@sentry.io>
2023-09-11 14:54:37 -0700

    Better error handling for getTeams function (#603)
    * better error handling for getting team info

--- src/utils/getTeams.ts
+++ src/utils/getTeams.ts
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/node';
+
 import { GH_ORGS, PRODUCT_OWNERS_YML } from '@/config';
 
 export function getTeams(
@@ -6,11 +8,23 @@ export function getTeams(
   org: string
 ): string[] {
   const orgObj = GH_ORGS.get(org);
+  // TODO: include codecov
+  if (org === 'codecov') {
+    return [];
+  }
   if (orgObj.repos.withoutRouting.includes(`${repo}`)) {
+    if (!PRODUCT_OWNERS_YML['repos'][repo]) {
+      Sentry.captureMessage(`Teams is not defined for ${org}/${repo}`);
+      return [];
+    }
     return [PRODUCT_OWNERS_YML['repos'][repo]];
   }
   if (orgObj.repos.withRouting.includes(`${repo}`)) {
     if (productArea) {
+      if (!PRODUCT_OWNERS_YML['product_areas'][productArea]) {
+        Sentry.captureMessage(`Teams is not defined for ${productArea}`);
+        return [];
+      }
       return PRODUCT_OWNERS_YML['product_areas'][productArea];
     }
   }
