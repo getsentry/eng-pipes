b2d9f55896fd05fcea5bd85cb8a9ec740247be91
Athena Moghaddam <athena.moghaddam@sentry.io>
2023-09-13 15:27:43 -0700

    add data

--- src/brain/apis/getProgress.ts
+++ src/brain/apis/getProgress.ts
@@ -1,13 +1,49 @@
 import { Octokit } from '@octokit/rest';
 
-export default async function getProgress() {
+function get_emoji_for_type(type, api_rate) {
+  switch (type) {
+    case "public":
+      return api_rate == 0 ? ":sad_blob:" : api_rate < 20 ? ":blob-unamused:" : api_rate > 50 ? ":party-sunglasses-blob:" : "";
+    case "private":
+      return "";
+    case "unknown": case "experimental":
+      return api_rate == 0 ? ":party-sunglasses-blob:" : api_rate > 50 ? ":sad_blob:" : ":blob-unamused:";
+  }
+  return "";
+}
+
+function get_message_line_for_type(team_data, type, total) {
+  const api_rate = Math.round(team_data[type].length* 100/total)
+  const emoji = get_emoji_for_type(type, api_rate)
+  return `• ${type}: ${team_data[type].length} (${api_rate}%) ${emoji} \n`
+}
+
+function get_message_for_team(ownership_data, team) {
+  const team_data = ownership_data[team];
+  const total = 
+      team_data["public"].length + 
+      team_data["private"].length + 
+      team_data["experimental"].length + 
+      team_data["unknown"].length; 
+  return {
+    message: `Publish status for ${team} APIs:\n` +
+      get_message_line_for_type(team_data, 'public', total) + 
+      get_message_line_for_type(team_data, 'private', total) +
+      get_message_line_for_type(team_data, 'experimental', total) + 
+      get_message_line_for_type(team_data, 'unknown', total),
+    should_show_docs: team_data["unknown"].length + team_data["experimental"].length > 0
+  }
+}
+
+export default async function getProgress(team: string) {
   const octokitWithToken = new Octokit({
     auth: process.env.GITHUB_PERSONAL_TOKEN,
-  });
+  });  
+  
   const resp = await octokitWithToken.rest.repos.getContent({
     owner: 'getsentry',
     repo: 'sentry',
-    path: 'prettier.config.js',
+    path: 'src/sentry/apidocs/api_ownership_stats_dont_modify.json',
   });
 
   if (!('content' in resp.data)) {
@@ -21,5 +57,15 @@ export default async function getProgress() {
   }
 
   const buff = Buffer.from(resp.data.content, 'base64');
-  return buff.toString('ascii').trim();
+  const ownership_data = JSON.parse(buff.toString('ascii').trim());
+  if (team == '') {
+    // TODO: show data for all the teams
+    return 'Under Construction';
+  } 
+  
+  if (ownership_data.hasOwnProperty(team)) {
+    return get_message_for_team(ownership_data, team).message;
+  }
+  
+  return 'INVALID';
 }
--- src/brain/apis/index.ts
+++ src/brain/apis/index.ts
@@ -11,6 +11,8 @@ export function apis() {
         return;
       }
 
+      const params = event.text.trim().split("apis ")
+      const team = params.length == 2 ? params[1] : '';
       const [message, ...resps] = await Promise.all([
         say({
           text: 'fetching status ...',
@@ -26,23 +28,41 @@ export function apis() {
             },
           ],
         }),
-        getProgress(),
+        getProgress(team),
       ]);
 
-      await client.chat.update({
-        channel: String(message.channel),
-        ts: String(message.ts),
-        text: `Done`,
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: resps,
-            },
-          },
-        ],
-      });
+      if (resps.includes('INVALID')) {
+        await client.chat.update({
+            channel: String(message.channel),
+            ts: String(message.ts),
+            text: `Team name is not valid.`,
+            blocks: [
+                {
+                    type: 'section',
+                    text: {
+                    type: 'mrkdwn',
+                    text: 'Team name is not valid or does not own any APIs. Review <https://github.com/getsentry/sentry/blob/master/src/sentry/apidocs/api_ownership_stats_dont_modify.json|ownerhisp> and <https://develop.sentry.dev/api/public/#1-declaring-owner-for-the-endpoint|update on the endpoints> if needed. ',
+                    },
+                },
+            ],
+          });
+      } else {
+        await client.chat.update({
+            channel: String(message.channel),
+            ts: String(message.ts),
+            text: `Done`,
+            blocks: [
+              {
+                type: 'section',
+                text: {
+                  type: 'mrkdwn',
+                  text: String(resps),
+                },
+              },
+            ],
+          });
+      }
+     
     })
   );
 }
