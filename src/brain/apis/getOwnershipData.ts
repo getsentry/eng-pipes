b428a2e797d9d6401b897b3e4780438b43c1f409
Athena Moghaddam <athena.moghaddam@sentry.io>
2023-10-10 15:41:41 -0700

    add ;

--- src/brain/apis/getOwnershipData.ts
+++ src/brain/apis/getOwnershipData.ts
@@ -1,7 +1,7 @@
 import { GETSENTRY_ORG } from '@/config';
 
 const OWNERSHIP_FILE_PATH = 'api_ownership_stats_dont_modify.json';
-const SENTRY_API_SCHEMA = 'sentry-api-schema'
+const SENTRY_API_SCHEMA = 'sentry-api-schema';
 
 export default async function getOwnershipData() {
   const resp = await GETSENTRY_ORG.api.rest.repos.getContent({
