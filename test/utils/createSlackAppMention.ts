3e2f245a839f356aaae7d451b631216c6aa9aded
Hubert Deng <hubertdeng123@gmail.com>
2023-11-02 12:51:50 -0700

    trigger slack scores from ospo channel

--- test/utils/createSlackAppMention.ts
+++ test/utils/createSlackAppMention.ts
@@ -1,7 +1,12 @@
 import { createSlackEvent } from './createSlackEvent';
 
-export async function createSlackAppMention(fastify, text: string) {
+export async function createSlackAppMention(
+  fastify,
+  text: string,
+  channel?: string
+) {
   return await createSlackEvent(fastify, 'app_mention', {
     text,
+    channel,
   });
 }
