6640427c4b4ca7af7e152fa857bdaea8b6ed6516
Neo Huang <126607112+kneeyo1@users.noreply.github.com>
2023-12-20 14:58:47 -0800

    Uploading the original payload to sentry to better capture errors (#728)
    * Uploading the original payload to sentry to better capture errors
    
    * testcases
    
    * fixing test case
    
    * fixing test case
    
    * fixing test cases
    
    * rename
    
    * oops forgot to add this
    
    * fix
    
    * fix
    
    * ok capturing if anything goes wrong as a sentry erro
    
    * Delete test/payloads/sentry-options/testbadPayload.json
    
    * Delete test/payloads/sentry-options/testBadPayload.json
    
    * weird bug
    
    * reformat
    
    * fix
    
    * fix
    
    * adding in a spy for context
    
    * more thorough test

--- src/webhooks/sentry-options/sentry-options.ts
+++ src/webhooks/sentry-options/sentry-options.ts
@@ -43,6 +43,7 @@ export async function sendSentryOptionsUpdatesToDatadog(
       ? 'success'
       : 'error';
   };
+  const region = formatRegionTag(message.region);
 
   for (const optionType in message) {
     if (optionType === 'region' || optionType === 'source') continue;
@@ -51,8 +52,6 @@ export async function sendSentryOptionsUpdatesToDatadog(
         change: optionType,
         option: option,
       };
-
-      const region = formatRegionTag(message.region);
 
       const alertType = formatAlertType(optionType);
 
@@ -80,117 +79,117 @@ export async function messageSlack(message: SentryOptionsResponse) {
   if (message.source !== 'options-automator') {
     return;
   }
-  const successBlock: KnownBlock[] = [
-    slackblocks.header(
-      slackblocks.plaintext(
-        message.region
-          ? `✅ Successfully Updated Options in ${message.region}: ✅`
-          : '✅ Successfully Updated Options ✅'
-      )
-    ),
-    ...(message.updated_options.length > 0
-      ? [
-          slackblocks.divider(),
-          slackblocks.section(slackblocks.markdown('*Updated options:* ')),
-          slackblocks.sectionBlock(
-            message.updated_options.map((option) =>
-              slackblocks.markdown(
-                `updated \`${option.option_name}\` with db value \`${option.db_value}\` to value \`${option.value}\``
-              )
-            )
-          ),
-        ]
-      : []),
-    ...(message.set_options.length > 0
-      ? [
-          slackblocks.divider(),
-          slackblocks.section(slackblocks.markdown('*Set Options:* ')),
-          slackblocks.sectionBlock(
-            message.set_options.map((option) =>
-              slackblocks.markdown(
-                `Set \`${option.option_name}\` to value \`${option.option_value}\``
-              )
-            )
-          ),
-        ]
-      : []),
-    ...(message.unset_options.length > 0
-      ? [
-          slackblocks.divider(),
-          slackblocks.section(slackblocks.markdown('*Unset Options:* ')),
-          slackblocks.sectionBlock(
-            message.unset_options.map((option) =>
-              slackblocks.markdown(`Unset \`${option}\``)
-            )
-          ),
-        ]
-      : []),
-  ];
-
-  const failedBlock: KnownBlock[] = [
-    slackblocks.header(
-      slackblocks.plaintext(
-        message.region
-          ? `❌ FAILED TO UPDATE in ${message.region}: ❌`
-          : '❌ FAILED TO UPDATE ❌'
-      )
-    ),
-    ...(message.drifted_options.length > 0
-      ? [
-          slackblocks.divider(),
-          slackblocks.section(slackblocks.markdown('*DRIFTED OPTIONS:* ')),
-          slackblocks.sectionBlock(
-            message.drifted_options.map((option) =>
-              slackblocks.markdown(
-                `\`${option.option_name}\` drifted. value on db: \`${option.option_value}\``
-              )
-            )
-          ),
-        ]
-      : []),
-    ...(message.not_writable_options.length > 0
-      ? [
-          slackblocks.divider(),
-          slackblocks.section(slackblocks.markdown('*FAILED:* ')),
-          slackblocks.sectionBlock(
-            message.not_writable_options.map((option) =>
-              slackblocks.markdown(
-                `FAILED TO UPDATE \`${option.option_name}\` \nREASON: \`${option.error_msg}\``
-              )
-            )
-          ),
-        ]
-      : []),
-    ...(message.unregistered_options.length > 0
-      ? [
-          slackblocks.divider(),
-          slackblocks.section(slackblocks.markdown('*Unregistered Options:* ')),
-          slackblocks.sectionBlock(
-            message.unregistered_options.map((option) =>
-              slackblocks.markdown(`Option \`${option}\` is not registered!`)
-            )
-          ),
-        ]
-      : []),
-    ...(message.invalid_type_options.length > 0
-      ? [
-          slackblocks.divider(),
-          slackblocks.section(
-            slackblocks.markdown('*Invalid Typed Options:* ')
-          ),
-          slackblocks.sectionBlock(
-            message.invalid_type_options.map((option) =>
-              slackblocks.markdown(
-                `Option \`${option.option_name}\` got type \`${option.got_type}\`, 
-                    but expected type \`${option.expected_type}\`.`
-              )
-            )
-          ),
-        ]
-      : []),
-  ];
-
   try {
+    const successBlock: KnownBlock[] = [
+      slackblocks.header(
+        slackblocks.plaintext(
+          message.region
+            ? `✅ Successfully Updated Options in ${message.region}: ✅`
+            : '✅ Successfully Updated Options ✅'
+        )
+      ),
+      ...(message.updated_options.length > 0
+        ? [
+            slackblocks.divider(),
+            slackblocks.section(slackblocks.markdown('*Updated options:* ')),
+            slackblocks.sectionBlock(
+              message.updated_options.map((option) =>
+                slackblocks.markdown(
+                  `updated \`${option.option_name}\` with db value \`${option.db_value}\` to value \`${option.value}\``
+                )
+              )
+            ),
+          ]
+        : []),
+      ...(message.set_options.length > 0
+        ? [
+            slackblocks.divider(),
+            slackblocks.section(slackblocks.markdown('*Set Options:* ')),
+            slackblocks.sectionBlock(
+              message.set_options.map((option) =>
+                slackblocks.markdown(
+                  `Set \`${option.option_name}\` to value \`${option.option_value}\``
+                )
+              )
+            ),
+          ]
+        : []),
+      ...(message.unset_options.length > 0
+        ? [
+            slackblocks.divider(),
+            slackblocks.section(slackblocks.markdown('*Unset Options:* ')),
+            slackblocks.sectionBlock(
+              message.unset_options.map((option) =>
+                slackblocks.markdown(`Unset \`${option}\``)
+              )
+            ),
+          ]
+        : []),
+    ];
+
+    const failedBlock: KnownBlock[] = [
+      slackblocks.header(
+        slackblocks.plaintext(
+          message.region
+            ? `❌ FAILED TO UPDATE in ${message.region}: ❌`
+            : '❌ FAILED TO UPDATE ❌'
+        )
+      ),
+      ...(message.drifted_options.length > 0
+        ? [
+            slackblocks.divider(),
+            slackblocks.section(slackblocks.markdown('*DRIFTED OPTIONS:* ')),
+            slackblocks.sectionBlock(
+              message.drifted_options.map((option) =>
+                slackblocks.markdown(
+                  `\`${option.option_name}\` drifted. value on db: \`${option.option_value}\``
+                )
+              )
+            ),
+          ]
+        : []),
+      ...(message.not_writable_options.length > 0
+        ? [
+            slackblocks.divider(),
+            slackblocks.section(slackblocks.markdown('*FAILED:* ')),
+            slackblocks.sectionBlock(
+              message.not_writable_options.map((option) =>
+                slackblocks.markdown(
+                  `FAILED TO UPDATE \`${option.option_name}\` \nREASON: \`${option.error_msg}\``
+                )
+              )
+            ),
+          ]
+        : []),
+      ...(message.unregistered_options.length > 0
+        ? [
+            slackblocks.divider(),
+            slackblocks.section(
+              slackblocks.markdown('*Unregistered Options:* ')
+            ),
+            slackblocks.sectionBlock(
+              message.unregistered_options.map((option) =>
+                slackblocks.markdown(`Option \`${option}\` is not registered!`)
+              )
+            ),
+          ]
+        : []),
+      ...(message.invalid_type_options.length > 0
+        ? [
+            slackblocks.divider(),
+            slackblocks.section(
+              slackblocks.markdown('*Invalid Typed Options:* ')
+            ),
+            slackblocks.sectionBlock(
+              message.invalid_type_options.map((option) =>
+                slackblocks.markdown(
+                  `Option \`${option.option_name}\` got type \`${option.got_type}\`, but expected type \`${option.expected_type}\`.`
+                )
+              )
+            ),
+          ]
+        : []),
+    ];
     if (successBlock.length > 1) {
       await bolt.client.chat.postMessage({
         channel: FEED_OPTIONS_AUTOMATOR_CHANNEL_ID,
@@ -208,6 +207,7 @@ export async function messageSlack(message: SentryOptionsResponse) {
       });
     }
   } catch (err) {
+    Sentry.setContext('message_data', { message });
     Sentry.captureException(err);
   }
 }
