3a5628c733034ca96a2c36886487fc54e7b3f7f4
Alexander Tarasov <alex.tarasov@sentry.io>
2024-01-16 16:18:45 +0100

    use Secrets Manager references instead of plain env variables

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,26 +4,36 @@ PROJECT=super-big-data
 IMAGE=gcr.io/${PROJECT}/product-eng-webhooks
 
 env_vars="ENV=production,"
-env_vars="${env_vars}GH_APP_PRIVATE_KEY_FOR_GETSENTRY=${GH_APP_PRIVATE_KEY_FOR_GETSENTRY},"
-env_vars="${env_vars}GH_APP_PRIVATE_KEY_FOR_CODECOV=${GH_APP_PRIVATE_KEY_FOR_CODECOV},"
-env_vars="${env_vars}DD_API_KEY=${DD_API_KEY},"
-env_vars="${env_vars}DD_APP_KEY=${DD_APP_KEY},"
-env_vars="${env_vars}GH_WEBHOOK_SECRET=${GH_WEBHOOK_SECRET},"
-env_vars="${env_vars}SLACK_SIGNING_SECRET=${SLACK_SIGNING_SECRET},"
-env_vars="${env_vars}SLACK_BOT_USER_ACCESS_TOKEN=${SLACK_BOT_USER_ACCESS_TOKEN},"
 env_vars="${env_vars}VERSION=${VERSION},"
-env_vars="${env_vars}SENTRY_WEBPACK_WEBHOOK_SECRET=${SENTRY_WEBPACK_WEBHOOK_SECRET},"
 env_vars="${env_vars}DB_USER=${DB_USER},"
 env_vars="${env_vars}DB_NAME=${DB_NAME},"
-env_vars="${env_vars}DB_PASSWORD=${DB_PASSWORD},"
 env_vars="${env_vars}DB_INSTANCE_CONNECTION_NAME=${DB_INSTANCE_CONNECTION_NAME},"
-env_vars="${env_vars}GH_USER_TOKEN=${GH_USER_TOKEN},"
 
+secret_names="
+GH_APP_PRIVATE_KEY_FOR_GETSENTRY
+GH_APP_PRIVATE_KEY_FOR_CODECOV
+DD_API_KEY
+DD_APP_KEY
+GH_WEBHOOK_SECRET
+SLACK_SIGNING_SECRET
+SLACK_BOT_USER_ACCESS_TOKEN
+SENTRY_WEBPACK_WEBHOOK_SECRET
+DB_PASSWORD
+GH_USER_TOKEN
+"
+
+secrets=""
+for secret_name in $secret_names; do
+  secrets="${secrets}${secret_name}=eng-pipes-${secret_name},"
+done
+
+# secrets="DD_API_KEY=eng-pipes-DD_API_KEY:latest,"
 
 gcloud builds submit --tag $IMAGE --project=$PROJECT --gcs-log-dir=gs://${PROJECT}_cloudbuild/logs && \
 gcloud run deploy product-eng-webhooks \
   --image $IMAGE \
   --set-env-vars="$env_vars" \
+  --set-secrets="$secrets" \
   --project=$PROJECT \
   --platform managed \
   --allow-unauthenticated \
