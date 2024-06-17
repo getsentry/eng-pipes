92e33af363c886f2279b28835f400091b6601807
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-06-17 15:37:50 -0700

    Fixing auth headers

--- src/utils/iap.test.ts
+++ src/utils/iap.test.ts
@@ -1,13 +1,13 @@
 import { GoogleAuth, IdTokenClient } from 'google-auth-library';
 
-import { IAP_TARGET_AUDIENCE } from '@/config';
+import { GOCD_TOKEN, IAP_TARGET_AUDIENCE } from '@/config';
 
 import { fetchUsingProxyAuth } from './iap';
 
 describe('iap', () => {
   it('fetches using proxy auth', async () => {
     const mockGetRequestHeaders = jest.fn().mockResolvedValue({
-      Authorization: 'Bearer blah',
+      Authorization: `Bearer ${GOCD_TOKEN}`,
     });
     const mockGetIdTokenClient = jest.fn().mockResolvedValue({
       getRequestHeaders: mockGetRequestHeaders,
@@ -19,7 +19,7 @@ describe('iap', () => {
     await fetchUsingProxyAuth('https://example.com', {
       method: 'GET',
       headers: {
-        Authorization: 'Bearer blah',
+        Authorization: `Bearer ${GOCD_TOKEN}`,
       },
     });
 
