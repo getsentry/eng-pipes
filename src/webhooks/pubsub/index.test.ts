d298c8e3e2661e162fc2f209dbbf643885fe5465
Hubert Deng <hubertdeng123@gmail.com>
2024-07-29 17:00:56 -0700

    add auth

--- src/webhooks/pubsub/index.test.ts
+++ src/webhooks/pubsub/index.test.ts
@@ -1,93 +1,103 @@
-// import * as gocdPausedPipelineBot from './gocdPausedPipelineBot';
-// import * as slackNotifications from './slackNotifications';
-// import * as slackScores from './slackScores';
-// import * as staleBot from './stalebot';
-// import { pubSubHandler } from '.';
+import { OAuth2Client } from 'google-auth-library';
 
-// const mockGocdPausedPipelineBot = jest.fn();
-// const mockNotifier = jest.fn();
-// const mockSlackScores = jest.fn();
-// const mockStaleBot = jest.fn();
+import * as gocdPausedPipelineBot from './gocdPausedPipelineBot';
+import * as slackNotifications from './slackNotifications';
+import * as slackScores from './slackScores';
+import * as staleBot from './stalebot';
+import { pubSubHandler } from '.';
 
-// gocdPausedPipelineBot.triggerPausedPipelineBot = mockGocdPausedPipelineBot;
-// slackNotifications.notifyProductOwnersForUntriagedIssues = mockNotifier;
-// slackScores.triggerSlackScores = mockSlackScores;
-// staleBot.triggerStaleBot = mockStaleBot;
+const mockGocdPausedPipelineBot = jest.fn();
+const mockNotifier = jest.fn();
+const mockSlackScores = jest.fn();
+const mockStaleBot = jest.fn();
 
-// class MockReply {
-//   statusCode: number;
-//   code(c) {
-//     this.statusCode = c;
-//   }
-//   send() {}
-// }
+gocdPausedPipelineBot.triggerPausedPipelineBot = mockGocdPausedPipelineBot;
+slackNotifications.notifyProductOwnersForUntriagedIssues = mockNotifier;
+slackScores.triggerSlackScores = mockSlackScores;
+staleBot.triggerStaleBot = mockStaleBot;
+
+class MockReply {
+  statusCode: number;
+  code(c) {
+    this.statusCode = c;
+  }
+  send() {}
+}
 
 describe('slack app', function () {
-  it('noop', () => {});
-  // async function pubSub(operationSlug: string) {
-  //   const request = {
-  //     body: {
-  //       message: {
-  //         data: new Buffer.from(
-  //           JSON.stringify({ name: operationSlug })
-  //         ).toString('base64'),
-  //       },
-  //     },
-  //   };
-  //   const reply = new MockReply();
-  //   await pubSubHandler(request, reply);
-  //   return reply;
-  // }
+  async function pubSub(operationSlug: string) {
+    const request = {
+      body: {
+        message: {
+          data: new Buffer.from(
+            JSON.stringify({ name: operationSlug })
+          ).toString('base64'),
+        },
+      },
+      headers: {
+        authorization: 'Bearer 1234abcd',
+      },
+    };
+    const reply = new MockReply();
+    await pubSubHandler(request, reply);
+    return reply;
+  }
 
-  // beforeEach(async function () {
-  //   mockGocdPausedPipelineBot.mockClear();
-  //   mockNotifier.mockClear();
-  //   mockSlackScores.mockClear();
-  //   mockStaleBot.mockClear();
-  // });
+  beforeAll(function () {
+    jest
+      .spyOn(OAuth2Client.prototype, 'verifyIdToken')
+      .mockImplementation(jest.fn());
+  });
 
-  // it('basically works', async function () {
-  //   const reply = await pubSub('stale-bot');
-  //   expect(reply.statusCode).toBe(204);
-  //   expect(mockGocdPausedPipelineBot).not.toHaveBeenCalled();
-  //   expect(mockNotifier).not.toHaveBeenCalled();
-  //   expect(mockSlackScores).not.toHaveBeenCalled();
-  //   expect(mockStaleBot).toHaveBeenCalled();
-  // });
+  beforeEach(async function () {
+    mockGocdPausedPipelineBot.mockClear();
+    mockNotifier.mockClear();
+    mockSlackScores.mockClear();
+    mockStaleBot.mockClear();
+  });
 
-  // it('keeps working', async function () {
-  //   const reply = await pubSub('stale-triage-notifier');
-  //   expect(reply.statusCode).toBe(204);
-  //   expect(mockGocdPausedPipelineBot).not.toHaveBeenCalled();
-  //   expect(mockNotifier).toHaveBeenCalled();
-  //   expect(mockSlackScores).not.toHaveBeenCalled();
-  //   expect(mockStaleBot).not.toHaveBeenCalled();
-  // });
+  it('basically works', async function () {
+    const reply = await pubSub('stale-bot');
+    expect(reply.statusCode).toBe(204);
+    expect(mockGocdPausedPipelineBot).not.toHaveBeenCalled();
+    expect(mockNotifier).not.toHaveBeenCalled();
+    expect(mockSlackScores).not.toHaveBeenCalled();
+    expect(mockStaleBot).toHaveBeenCalled();
+  });
 
-  // it('works for slack-scores', async function () {
-  //   const reply = await pubSub('slack-scores');
-  //   expect(reply.statusCode).toBe(204);
-  //   expect(mockGocdPausedPipelineBot).not.toHaveBeenCalled();
-  //   expect(mockNotifier).not.toHaveBeenCalled();
-  //   expect(mockSlackScores).toHaveBeenCalled();
-  //   expect(mockStaleBot).not.toHaveBeenCalled();
-  // });
+  it('keeps working', async function () {
+    const reply = await pubSub('stale-triage-notifier');
+    expect(reply.statusCode).toBe(204);
+    expect(mockGocdPausedPipelineBot).not.toHaveBeenCalled();
+    expect(mockNotifier).toHaveBeenCalled();
+    expect(mockSlackScores).not.toHaveBeenCalled();
+    expect(mockStaleBot).not.toHaveBeenCalled();
+  });
 
-  // it('works for gocd-paused-pipeline-bot', async function () {
-  //   const reply = await pubSub('gocd-paused-pipeline-bot');
-  //   expect(reply.statusCode).toBe(204);
-  //   expect(mockGocdPausedPipelineBot).toHaveBeenCalled();
-  //   expect(mockNotifier).not.toHaveBeenCalled();
-  //   expect(mockSlackScores).not.toHaveBeenCalled();
-  //   expect(mockStaleBot).not.toHaveBeenCalled();
-  // });
+  it('works for slack-scores', async function () {
+    const reply = await pubSub('slack-scores');
+    expect(reply.statusCode).toBe(204);
+    expect(mockGocdPausedPipelineBot).not.toHaveBeenCalled();
+    expect(mockNotifier).not.toHaveBeenCalled();
+    expect(mockSlackScores).toHaveBeenCalled();
+    expect(mockStaleBot).not.toHaveBeenCalled();
+  });
 
-  // it('replies with 400 for unknown operations', async function () {
-  //   const reply = await pubSub('cheez-whiz');
-  //   expect(reply.statusCode).toBe(400);
-  //   expect(mockGocdPausedPipelineBot).not.toHaveBeenCalled();
-  //   expect(mockNotifier).not.toHaveBeenCalled();
-  //   expect(mockSlackScores).not.toHaveBeenCalled();
-  //   expect(mockStaleBot).not.toHaveBeenCalled();
-  // });
+  it('works for gocd-paused-pipeline-bot', async function () {
+    const reply = await pubSub('gocd-paused-pipeline-bot');
+    expect(reply.statusCode).toBe(204);
+    expect(mockGocdPausedPipelineBot).toHaveBeenCalled();
+    expect(mockNotifier).not.toHaveBeenCalled();
+    expect(mockSlackScores).not.toHaveBeenCalled();
+    expect(mockStaleBot).not.toHaveBeenCalled();
+  });
+
+  it('replies with 400 for unknown operations', async function () {
+    const reply = await pubSub('cheez-whiz');
+    expect(reply.statusCode).toBe(400);
+    expect(mockGocdPausedPipelineBot).not.toHaveBeenCalled();
+    expect(mockNotifier).not.toHaveBeenCalled();
+    expect(mockSlackScores).not.toHaveBeenCalled();
+    expect(mockStaleBot).not.toHaveBeenCalled();
+  });
 });
