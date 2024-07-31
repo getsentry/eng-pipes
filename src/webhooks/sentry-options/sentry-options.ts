a8394df0143a4ebe126d66012ec9c3f09a96a9d8
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-07-31 11:23:26 -0700

    Be more explicit about sending status

--- src/webhooks/sentry-options/sentry-options.ts
+++ src/webhooks/sentry-options/sentry-options.ts
@@ -32,16 +32,19 @@ export async function handler(
     );
 
     if (!isVerified) {
+      // If the signature is not verified, return (since extractAndVerifySignature sends the response)
       return;
     }
 
     const { body }: { body: SentryOptionsResponse } = request;
     await messageSlack(body);
     await sendSentryOptionsUpdatesToDatadog(body, moment().unix());
-    return reply.code(200).send('OK');
+    reply.code(200).send('OK');
+    return;
   } catch (err) {
     Sentry.captureException(err);
-    return reply.code(500).send();
+    reply.code(500).send();
+    return;
   }
 }
 
