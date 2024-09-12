b6a0acda66abb42cc5d45b24a8e6eebf803ab025
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-09-12 15:38:04 -0700

    fix(test): Fixing typo in test case

--- src/blocks/getUpdatedDeployMessage.test.ts
+++ src/blocks/getUpdatedDeployMessage.test.ts
@@ -10,13 +10,13 @@ describe('getUpdatedGoCDDeployMessage', function () {
       currentUser: CURRENT_USER,
       stage_name: 'example-stage',
       state: 'Building',
-      want: `You have begun deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/undefined/example-stage/3|example-pipeline: Stage 3>)`,
+      want: `You have begun deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/2/example-stage/3|example-pipeline: Stage 3>)`,
     },
     {
       currentUser: 'diff-user',
       stage_name: 'example-stage',
       state: 'Building',
-      want: `<@diff-user> has begun deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/undefined/example-stage/3|example-pipeline: Stage 3>)`,
+      want: `<@diff-user> has begun deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/2/example-stage/3|example-pipeline: Stage 3>)`,
     },
 
     // Passed an intermediate stage
@@ -24,13 +24,13 @@ describe('getUpdatedGoCDDeployMessage', function () {
       currentUser: CURRENT_USER,
       stage_name: 'example-stage',
       state: 'Passed',
-      want: `You have begun deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/undefined/example-stage/3|example-pipeline: Stage 3>)`,
+      want: `You have begun deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/2/example-stage/3|example-pipeline: Stage 3>)`,
     },
     {
       currentUser: 'diff-user',
       stage_name: 'example-stage',
       state: 'Passed',
-      want: `<@diff-user> has begun deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/undefined/example-stage/3|example-pipeline: Stage 3>)`,
+      want: `<@diff-user> has begun deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/2/example-stage/3|example-pipeline: Stage 3>)`,
     },
 
     // Passed final stage
@@ -38,13 +38,13 @@ describe('getUpdatedGoCDDeployMessage', function () {
       currentUser: CURRENT_USER,
       stage_name: FINAL_STAGE_NAMES[0],
       state: 'Passed',
-      want: `You have finished deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/undefined/${FINAL_STAGE_NAMES[0]}/3|example-pipeline: Stage 3>)`,
+      want: `You have finished deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/2/${FINAL_STAGE_NAMES[0]}/3|example-pipeline: Stage 3>)`,
     },
     {
       currentUser: 'diff-user',
       stage_name: FINAL_STAGE_NAMES[0],
       state: 'Passed',
-      want: `<@diff-user> has finished deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/undefined/${FINAL_STAGE_NAMES[0]}/3|example-pipeline: Stage 3>)`,
+      want: `<@diff-user> has finished deploying this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/2/${FINAL_STAGE_NAMES[0]}/3|example-pipeline: Stage 3>)`,
     },
 
     // Unknown / Failed
@@ -52,13 +52,13 @@ describe('getUpdatedGoCDDeployMessage', function () {
       currentUser: CURRENT_USER,
       stage_name: 'example-stage',
       state: 'Other',
-      want: `You have failed to deploy this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/undefined/example-stage/3|example-pipeline: Stage 3>)`,
+      want: `You have failed to deploy this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/2/example-stage/3|example-pipeline: Stage 3>)`,
     },
     {
       currentUser: 'diff-user',
       stage_name: 'example-stage',
       state: 'Other',
-      want: `<@diff-user> has failed to deploy this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/undefined/example-stage/3|example-pipeline: Stage 3>)`,
+      want: `<@diff-user> has failed to deploy this commit (<${GOCD_ORIGIN}/go/pipelines/example-pipeline/2/example-stage/3|example-pipeline: Stage 3>)`,
     },
   ];
 
@@ -69,7 +69,7 @@ describe('getUpdatedGoCDDeployMessage', function () {
         slackUser: s.currentUser,
         pipeline: {
           pipeline_name: 'example-pipeline',
-          pipline_counter: 2,
+          pipeline_counter: 2,
           stage_name: s.stage_name,
           stage_counter: 3,
           stage_state: s.state,
