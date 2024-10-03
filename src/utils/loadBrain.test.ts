304a1ce6dc175e87684ded175a60c535a59ac8e2
Brian Lou <brian.lou@sentry.io>
2024-10-03 14:40:52 -0700

    Make loadBrain support nested dirs

--- src/utils/loadBrain.test.ts
+++ src/utils/loadBrain.test.ts
@@ -5,8 +5,9 @@ jest.unmock('./loadBrain');
 describe('loadBrain', function () {
   it('makes sure that for every file in `brain/`, we load at least one function from it', async function () {
     const modules = await getBrainModules();
+    const modulesSet = new Set(modules.map((m) => m.split('/').pop())); // Get the filename from the path
     const fns = getExportedFunctions(modules);
-
-    expect(new Set(modules)).toEqual(new Set(fns.map((f) => f.name)));
+    const t = new Set(fns.map((f) => f.name));
+    expect(modulesSet).toEqual(t); // Check that the filenames & loaded functions are the same
   });
 });
