b15d743da361cc873cf4993d1699245ee0e1882a
Brian Lou <brian.lou@sentry.io>
2024-10-16 10:14:10 -0700

    Add txn logging

--- src/webhooks/index.ts
+++ src/webhooks/index.ts
@@ -9,38 +9,47 @@ import { kafkactlWebhook } from './kafka-control-plane/kafka-control-plane';
 import { sentryOptionsWebhook } from './sentry-options/sentry-options';
 import { webpackWebhook } from './webpack/webpack';
 
+type WebhookHandler = (
+  request: FastifyRequest<any>,
+  reply: FastifyReply
+) => Promise<void>;
+
 // Error handling wrapper function
 export async function handleRoute(
-  handler,
+  handler: WebhookHandler,
   request: FastifyRequest,
-  reply: FastifyReply
+  reply: FastifyReply,
+  name: string
 ): Promise<void> {
+  const tx = Sentry.startTransaction({
+    op: 'webhooks',
+    name: 'webhooks.' + name,
+  });
   try {
     await handler(request, reply);
   } catch (err) {
-    console.error(err);
     Sentry.captureException(err);
     reply.code(400).send('Bad Request');
-    return;
   }
+  tx.finish();
 }
 
 // Function that maps routes to their respective handlers
 export async function routeHandlers(server: Fastify, _options): Promise<void> {
   server.post('/metrics/bootstrap-dev-env/webhook', (request, reply) =>
-    handleRoute(bootstrapWebhook, request, reply)
+    handleRoute(bootstrapWebhook, request, reply, 'bootstrap-dev-env')
   );
   server.post('/metrics/gocd/webhook', (request, reply) =>
-    handleRoute(gocdWebhook, request, reply)
+    handleRoute(gocdWebhook, request, reply, 'gocd')
   );
   server.post('/metrics/kafka-control-plane/webhook', (request, reply) =>
-    handleRoute(kafkactlWebhook, request, reply)
+    handleRoute(kafkactlWebhook, request, reply, 'kafka-control-plane')
   );
   server.post('/metrics/sentry-options/webhook', (request, reply) =>
-    handleRoute(sentryOptionsWebhook, request, reply)
+    handleRoute(sentryOptionsWebhook, request, reply, 'sentry-options')
   );
   server.post('/metrics/webpack/webhook', (request, reply) =>
-    handleRoute(webpackWebhook, request, reply)
+    handleRoute(webpackWebhook, request, reply, 'webpack')
   );
 
   // Default handler for invalid routes
